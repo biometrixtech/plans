caad4565c9634e86cfef2d93f90256058298c629
Stephen Poole <stephen@melon.software>
2018-07-11 23:28:03 +0100

    Fixes to soreness being dictionaries vs DailySoreness objects

--- apigateway/datastores/daily_readiness_datastore.py
+++ apigateway/datastores/daily_readiness_datastore.py
@@ -1,5 +1,6 @@
 from aws_xray_sdk.core import xray_recorder
 from config import get_mongo_collection
+from logic.soreness_and_injury import DailySoreness
 from models.daily_readiness import DailyReadiness
 from utils import format_datetime
 
@@ -24,23 +25,26 @@ class DailyReadinessDatastore(object):
     def _query_mongodb(self, user_id):
         mongo_collection = get_mongo_collection(self.mongo_collection)
         query = {'user_id': user_id}
-        mongo_result = mongo_collection.find_one(query, sort=[('date_time', -1)])
-        print(mongo_result)
+        mongo_result = mongo_collection.find_one(query, sort=[('event_date', -1)])
         return self.item_from_mongodb(mongo_result)
 
     @xray_recorder.capture('datastore.DailyReadinessDatastore._put_mongodb')
     def _put_mongodb(self, item):
         item = self.item_to_mongodb(item)
         mongo_collection = get_mongo_collection(self.mongo_collection)
-        query = {'user_id': item['user_id'], 'date_time': format_datetime(item['date_time'])}
+        query = {'user_id': item['user_id'], 'event_date': format_datetime(item['event_date'])}
         mongo_collection.replace_one(query, item, upsert=True)
 
     @staticmethod
     def item_to_mongodb(dailyreadiness):
+        """
+        :param DailyReadiness dailyreadiness:
+        :return: dict
+        """
         item = {
-            'date_time': dailyreadiness.date_time,
+            'event_date': dailyreadiness.event_date,
             'user_id': dailyreadiness.user_id,
-            'soreness': dailyreadiness.get_soreness(),
+            'soreness': [s.json_serialise() for s in dailyreadiness.soreness],
             'sleep_quality': dailyreadiness.sleep_quality,
             'readiness': dailyreadiness.readiness,
         }
@@ -48,10 +52,22 @@ class DailyReadinessDatastore(object):
 
     @staticmethod
     def item_from_mongodb(mongo_result):
+        """
+        :param dict mongo_result:
+        :return: DailyReadiness
+        """
         return DailyReadiness(
             user_id=mongo_result['user_id'],
-            date_time=mongo_result['date_time'],
-            soreness=mongo_result['soreness'],  # dailysoreness object array
+            event_date=mongo_result['event_date'],
+            soreness=[_soreness_from_mongodb(s, mongo_result) for s in mongo_result['soreness']],
             sleep_quality=mongo_result['sleep_quality'],
             readiness=mongo_result['readiness']
         )
+
+
+def _soreness_from_mongodb(soreness_mongo_result, parent_mongo_result):
+    soreness = DailySoreness()
+    soreness.body_part = soreness_mongo_result['body_part']
+    soreness.severity = soreness_mongo_result['severity']
+    soreness.reported_date_time = parent_mongo_result['event_date']
+    return soreness
--- apigateway/models/daily_readiness.py
+++ apigateway/models/daily_readiness.py
@@ -32,15 +32,6 @@ class DailyReadiness(Serialisable):
             'soreness': self.soreness,
             'sleep_quality': self.sleep_quality,
             'readiness': self.readiness,
-            'sore_body_parts': [s['body_part'] for s in self.soreness if s['severity'] > 1]
+            'sore_body_parts': [s.body_part for s in self.soreness if s.severity > 1]
         }
         return ret
-
-    def get_soreness(self):
-        soreness = []
-        for body_part in self.soreness:
-            body_part_filt = {'body_part': body_part['body_part'],
-                              'severity': body_part['severity']}
-            soreness.append(body_part_filt)
-        return soreness
-
