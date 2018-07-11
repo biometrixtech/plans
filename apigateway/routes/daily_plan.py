e493337b9559f92ec542d04fe9c31c1b12ccc00d
DIpesh Gautam <dipesh@biometrixtech.com>
2018-07-11 08:58:11 -0400

    add data validation

--- apigateway/routes/daily_plan.py
+++ apigateway/routes/daily_plan.py
@@ -20,6 +20,7 @@ app = Blueprint('daily_plan', __name__)
 @authentication_required
 @xray_recorder.capture('routes.daily_plan.get')
 def handle_daily_plan_get():
+    validate_input(request)
 
     user_id = request.json['user_id']
     start_date = request.json['start_date']
@@ -30,4 +31,24 @@ def handle_daily_plan_get():
     store = DailyPlanDatastore()
     daily_plan = store.get(user_id, start_date, end_date, collection='dailyplan')
     return {'daily_plan': daily_plan}, 200
+
+
+def validate_input(request):
+    if not isinstance(request.json, dict):
+        raise InvalidSchemaException('Request body must be a dictionary')
+    if 'user_id' not in request.json:
+        raise InvalidSchemaException('Missing required parameter user_id')
+    if 'start_date' not in request.json:
+        raise InvalidSchemaException('Missing required parameter start_date')
+    else:
+        try:
+            datetime.datetime.strptime(request.json['start_date'], "%Y-%m-%d ")
+        except:
+            raise InvalidSchemaException('start_date needs to be in format yyyy-mm-dd')
+    if 'end_date' in request.json:
+        try:
+            datetime.datetime.strptime(request.json['end_date'], "%Y-%m-%d ")
+        except:
+            raise InvalidSchemaException('end_date needs to be in format yyyy-mm-dd')
+
 
