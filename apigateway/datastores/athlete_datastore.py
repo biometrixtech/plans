ed39745fa6a4ed7541c9584e28b46280f0ec8cb7
DIpesh Gautam <dipesh@biometrixtech.com>
2018-07-11 11:31:14 -0400

    try skipping use of dailyPlan object.

--- apigateway/datastores/athlete_datastore.py
+++ apigateway/datastores/athlete_datastore.py
@@ -16,23 +16,24 @@ class DailyPlanDatastore(object):
     def _query_mongodb(self, user_id, start_date, end_date, collection):
         mongo_collection = get_mongo_collection(collection)
         query0 = {'user_id': user_id, 'date': {'$gte': start_date, '$lte': end_date}}
-        query1 = {'_id': 0, 'last_updated': 0, 'user_id': 0}
-        mongo_cursor = mongo_collection.find(query0, query1)
+        # query1 = {'_id': 0, 'last_updated': 0, 'user_id': 0}
+        mongo_cursor = mongo_collection.find(query0)
         ret = []
         for plan in mongo_cursor:
-            daily_plan = DailyPlan(date=plan['date'])
-            daily_plan.practice_sessions = plan.get('practice_sessions', [])
-            daily_plan.strength_conditioning_sessions = plan.get('cross_training_sessions', [])
-            daily_plan.games = plan.get('game_sessions', [])
-            daily_plan.tournaments = plan.get('tournament_sessions', [])
-            daily_plan.recovery_am = plan.get('recovery_am', [])
-            daily_plan.recovery_pm = plan.get('recovery_pm', [])
-            daily_plan.corrective_sessions = plan.get('corrective_sessions', [])
-            daily_plan.bump_up_sessions = plan['bump_up_sessions']
-            daily_plan.daily_readiness_survey = plan.get('daily_readiness_survey', None)
-            daily_plan.updated = plan.get('updated', None)
-            daily_plan.last_updated = plan.get('last_update', None)
-            ret.append(daily_plan)
+            ret.append(self.item_to_response(plan))
+            # daily_plan = DailyPlan(date=plan['date'])
+            # daily_plan.practice_sessions = plan.get('practice_sessions', [])
+            # daily_plan.strength_conditioning_sessions = plan.get('cross_training_sessions', [])
+            # daily_plan.games = plan.get('game_sessions', [])
+            # daily_plan.tournaments = plan.get('tournament_sessions', [])
+            # daily_plan.recovery_am = plan.get('recovery_am', [])
+            # daily_plan.recovery_pm = plan.get('recovery_pm', [])
+            # daily_plan.corrective_sessions = plan.get('corrective_sessions', [])
+            # daily_plan.bump_up_sessions = plan['bump_up_sessions']
+            # daily_plan.daily_readiness_survey = plan.get('daily_readiness_survey', None)
+            # daily_plan.updated = plan.get('updated', None)
+            # daily_plan.last_updated = plan.get('last_update', None)
+            # ret.append(daily_plan)
         return ret
 
     @xray_recorder.capture('datastore.DailyPlanDatastore._put_mongodb')
@@ -43,13 +44,13 @@ class DailyPlanDatastore(object):
     def item_to_response(daily_plan):
         item = {
             'date': daily_plan['date'],
-            'practice_sessions': daily_plan['practice_sessions'],
-            'recovery_am': daily_plan['recovery_am'],
-            'recovery_pm': daily_plan['recovery_pm'],
-            'games': daily_plan['games'],
-            'tournaments': daily_plan['tournaments'],
-            'strength_conditioning_sessions': daily_plan['strength_conditioning_sessions'],
-            'corrective_sessions': daily_plan['corrective_sessions'],
-            'bump_up_sessions': daily_plan['bump_up_sessions'],
+            'practice_sessions': daily_plan.get('practice_sessions', []),
+            'recovery_am': daily_plan.get('recovery_am',[]),
+            'recovery_pm': daily_plan.get('recovery_pm', []),
+            'games': daily_plan.get('game_sessions', []),
+            'tournaments': daily_plan.get('tournament_sessions', []),
+            'strength_conditioning_sessions': daily_plan.get('cross_training_sessions', []),
+            'corrective_sessions': daily_plan.get('corrective_sessions', []),
+            'bump_up_sessions': daily_plan.get('bump_up_sessions', []),
         }
         return item
--- apigateway/routes/daily_plan.py
+++ apigateway/routes/daily_plan.py
@@ -24,9 +24,9 @@ def handle_daily_plan_get():
         end_date = start_date
     store = DailyPlanDatastore()
     daily_plan = store.get(user_id, start_date, end_date, collection='dailyplan')
-    daily_plans = []
-    for plan in daily_plan:
-        daily_plans.append(plan.item_to_response())
+    # daily_plans = []
+    # for plan in daily_plan:
+    #     daily_plans.append(plan)
 
     return {'daily_plan': daily_plan}, 200
 
