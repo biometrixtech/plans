b112eb8443cdbe8f337d3474cb70e46e1e2fa91c
Stephen Poole <stephen@melon.software>
2018-07-16 19:02:53 +0100

    Util function to run an endpoint asynchronously, by invoking the lambda function again

--- apigateway/utils.py
+++ apigateway/utils.py
@@ -1,4 +1,7 @@
+import boto3
 import datetime
+import json
+import os
 import uuid
 
 from exceptions import InvalidSchemaException
@@ -56,3 +59,38 @@ def validate_uuid4(uuid_string):
     except ValueError:
         # If it's a value error, then the string is not a valid hex code for a UUID.
         return False
+
+
+def run_async(endpoint, method='POST', body=None, headers=None):
+    endpoint = endpoint.strip('/')
+    base_headers = {
+        "Accept": "*/*",
+        "Authorization": get_service_token(),
+        "Content-Type": "application/json",
+        "Host": "apis.{}.fathomai.com".format(os.environ['ENVIRONMENT']),
+        "User-Agent": "Biometrix/Plans API",
+        "X-Forwarded-Port": "443",
+        "X-Forwarded-Proto": "https"
+    }
+    if isinstance(headers, dict):
+        base_headers.update(headers)
+    boto3.client('lambda').invoke(
+        FunctionName=os.environ.get('AWS_LAMBDA_FUNCTION_NAME', 'plans-{}-apigateway-execute'.format(os.environ['ENVIRONMENT'])),
+        InvocationType='Event',
+        Payload=json.dumps({
+            "path": "/plans/" + endpoint,
+            "httpMethod": method,
+            "headers": base_headers,
+            "queryStringParameters": None,
+            "pathParameters": {"endpoint": endpoint},
+            "stageVariables": None,
+            "requestContext": {"identity": {"sourceIp": "0.0.0.0"}},
+            "body": json.dumps(body) if body is not None else None,
+            "isBase64Encoded": False
+        }).encode(),
+    )
+
+
+def get_service_token():
+    # TODO
+    return "eyJraWQiOiJ1cy13ZXN0LTIxIiwidHlwIjoiSldTIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiJ1cy13ZXN0LTI6MTliZmFkNzUtOWQ5NS00ZmZmLWFlYzktZGU0YTkzZGEyMTRkIiwiYXVkIjoidXMtd2VzdC0yOjA5M2RhOGRjLThlNzUtNGJmNS04MGE5LWE0NmYwMDBhNGEyNiIsImFtciI6WyJ1bmF1dGhlbnRpY2F0ZWQiXSwiaXNzIjoiaHR0cHM6Ly9jb2duaXRvLWlkZW50aXR5LmFtYXpvbmF3cy5jb20iLCJleHAiOjE1MTM2MzA4ODcsImlhdCI6MTUxMzYzMDI4NywidXNlcl9pZCI6IjAyY2I3OTY1LTc5MjEtNDkzYS04MGQ0LTZiMjc4YzkyOGZhZCJ9.ZCUKZfP2XUZKeVjqx3txMzMBnFogZ65MdRqP1r27vTI"
