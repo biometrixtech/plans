2eb14fc12bab73e3736978a92312156d61e05a70
DIpesh Gautam <dipesh@biometrixtech.com>
2018-07-26 13:39:26 -0400

    Adding session information

--- apigateway/routes/daily_schedule.py
+++ apigateway/routes/daily_schedule.py
@@ -1,7 +1,5 @@
 from aws_xray_sdk.core import xray_recorder
 from flask import request, Blueprint
-import datetime
-import os
 
 from decorators import authentication_required
 from exceptions import InvalidSchemaException
@@ -9,7 +7,7 @@ from config import get_mongo_collection
 from datastores.daily_plan_datastore import DailyPlanDatastore
 from datastores.session_datastore import SessionDatastore, _create_session
 from models.daily_plan import DailyPlan
-
+from utils import format_date, format_datetime, parse_datetime
 app = Blueprint('daily_schedule', __name__)
 
 
@@ -17,9 +15,17 @@ app = Blueprint('daily_schedule', __name__)
 @authentication_required
 @xray_recorder.capture('routes.daily_schedule')
 def handle_daily_schedule_create():
+    if not isinstance(request.json, dict):
+        raise InvalidSchemaException('Request body must be a dictionary')
+    if 'event_date' not in request.json:
+        raise InvalidSchemaException('Missing required parameter event_date')
+    else:
+        event_date = format_date(request.json['event_date'])
+        if event_date is None:
+            raise InvalidSchemaException('event_date is not formatted correctly')
+
     user_id = request.json['user_id']
-    event_date = request.json['event_date']
-    if not check_plan_exists():
+    if not _check_plan_exists():
         plan = DailyPlan(event_date=event_date)
         plan.user_id = user_id
         DailyPlanDatastore().put(plan)
@@ -27,17 +33,29 @@ def handle_daily_schedule_create():
     sessions = request.json['sessions']
     session_store = SessionDatastore()
     for session in sessions:
+        session_data = _get_session_data(session)
         item = _create_session(user_id=user_id,
                                session_type=session['session_type'],
-                               data={'description':'test'})
+                               data=session_data)
         session_store.insert(item, user_id=user_id, event_date=event_date)
 
+    return {'message': 'success'}, 201
 
 
-def check_plan_exists():
+def _check_plan_exists():
     mongo_collection = get_mongo_collection('dailyplan')
     if mongo_collection.count({"user_id": request.json['user_id'],
                                "date": request.json['event_date']}) == 1:
         return True
     else:
         return False
+
+def _get_session_data(session):
+    date = format_date(parse_datetime(session['start_time']))
+    time = format_datetime(session['start_time'])
+
+    session_data = {"date": date,
+                   "time": time,
+                   "duration": session['duration']
+                   }
+    return session_data
