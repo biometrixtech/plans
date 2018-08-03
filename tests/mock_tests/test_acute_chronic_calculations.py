154b67db22e41f66274eaf44d8ce7a9dc9a5eb2c
Paul LaForge <paul@biometrixtech.com>
2018-08-03 13:11:51 -0600

    added calc tests and renamed a c tests

--- tests/mock_tests/test_acute_chronic_calculations.py
+++ tests/mock_tests/test_acute_chronic_calculations.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from models.daily_plan import DailyPlan
 from models.session import PracticeSession
 from models.daily_readiness import DailyReadiness
+from models.stats import AthleteStats
 from stats_processing import StatsProcessing
 from tests.testing_utilities import TestUtilities
 from tests.mocks.mock_athlete_stats_datastore import AthleteStatsDatastore
@@ -26,15 +27,30 @@ def get_daily_plans(start_date, end_date):
 
     dates = get_dates(start_date, end_date)
 
+    i = 1
+
     for d in dates:
         daily_plan = DailyPlan(event_date=d.strftime("%Y-%m-%dT%H:%M:%SZ"))
         practice_session = PracticeSession()
         practice_session.event_date = d
-        practice_session.external_load = 100
-        practice_session.high_intensity_load = 20
-        practice_session.mod_intensity_load = 50
-        practice_session.low_intensity_load = 30
+        practice_session.external_load = 10 * i
+        practice_session.high_intensity_load = 2 * i
+        practice_session.mod_intensity_load = 5 * i
+        practice_session.low_intensity_load = 3 * i
         daily_plan.practice_sessions.append(practice_session)
+        plans.append(daily_plan)
+        i += 1
+
+    return plans
+
+def get_sessionless_daily_plans(start_date, end_date):
+
+    plans = []
+
+    dates = get_dates(start_date, end_date)
+
+    for d in dates:
+        daily_plan = DailyPlan(event_date=d.strftime("%Y-%m-%dT%H:%M:%SZ"))
         plans.append(daily_plan)
 
     return plans
@@ -246,3 +262,36 @@ def test_chronic_correct_dates_40_days():
     assert weeks[3][0].event_date == '2018-05-30T12:00:00Z'
 
 
+def test_correct_acute_chronic_load_33_days():
+    plans = get_daily_plans(datetime(2018, 6, 1, 12, 0, 0), datetime(2018, 7, 3, 12, 0, 0))
+    surveys = get_daily_readiness_surveys(datetime(2018, 6, 1, 12, 0, 0), datetime(2018, 7, 3, 12, 0, 0))
+    daily_plan_datastore = DailyPlanDatastore()
+    daily_plan_datastore.side_load_plans(plans)
+    daily_readiness_datastore = DailyReadinessDatastore()
+    daily_readiness_datastore.side_load_surveys(surveys)
+
+    stats = StatsProcessing("Tester", "2018-07-03", daily_readiness_datastore, PostSessionSurveyDatastore(),
+                 daily_plan_datastore, AthleteStatsDatastore())
+    stats.set_start_end_times()
+    stats.load_acute_chronic_data()
+    athlete_stats = AthleteStats("Tester")
+    athlete_stats = stats.calc_training_volume_metrics(athlete_stats)
+    assert 2100 == athlete_stats.acute_external_total_load
+    assert 982.5 == athlete_stats.chronic_external_total_load
+
+def test_correct_acute_chronic_empty_load_33_days():
+    plans = get_sessionless_daily_plans(datetime(2018, 6, 1, 12, 0, 0), datetime(2018, 7, 3, 12, 0, 0))
+    surveys = get_daily_readiness_surveys(datetime(2018, 6, 1, 12, 0, 0), datetime(2018, 7, 3, 12, 0, 0))
+    daily_plan_datastore = DailyPlanDatastore()
+    daily_plan_datastore.side_load_plans(plans)
+    daily_readiness_datastore = DailyReadinessDatastore()
+    daily_readiness_datastore.side_load_surveys(surveys)
+
+    stats = StatsProcessing("Tester", "2018-07-03", daily_readiness_datastore, PostSessionSurveyDatastore(),
+                 daily_plan_datastore, AthleteStatsDatastore())
+    stats.set_start_end_times()
+    stats.load_acute_chronic_data()
+    athlete_stats = AthleteStats("Tester")
+    athlete_stats = stats.calc_training_volume_metrics(athlete_stats)
+    assert None is athlete_stats.acute_external_total_load
+    assert None is athlete_stats.chronic_external_total_load
