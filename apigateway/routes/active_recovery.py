4d5d51858b8abdf98930fd4ead83d709894eae8f
Paul LaForge <paul@biometrixtech.com>
2018-08-09 17:32:16 -0400

    save completed exercises

--- apigateway/routes/active_recovery.py
+++ apigateway/routes/active_recovery.py
@@ -4,6 +4,8 @@ from flask import request, Blueprint
 from decorators import authentication_required
 from exceptions import InvalidSchemaException, NoSuchEntityException
 from datastores.daily_plan_datastore import DailyPlanDatastore
+from datastores.completed_exercise_datastore import CompletedExerciseDatastore
+from models.exercise import CompletedExercise
 from utils import format_date
 from config import get_mongo_collection
 
@@ -33,17 +35,24 @@ def handle_active_recovery_update():
     if not _check_plan_exists(user_id, event_date):
         raise NoSuchEntityException('Plan not found for the user')
     store = DailyPlanDatastore()
+
     plan = store.get(user_id=user_id, start_date=event_date, end_date=event_date)[0]
     if recovery_type == 'pre':
         plan.pre_recovery_completed = True # plan
         plan.pre_recovery.completed = True # recovery
         plan.pre_recovery.display_exercises = False
+
     elif recovery_type == 'post':
         plan.post_recovery_completed = True # plan
         plan.post_recovery.completed = True # recovery
         plan.post_recovery.display_exercises = False
 
     store.put(plan)
+
+    if recovery_type == 'pre':
+        save_completed_exercises_for_recovery(plan.pre_recovery, user_id, event_date)
+    elif recovery_type == 'post':
+        save_completed_exercises_for_recovery(plan.post_recovery, user_id, event_date)
 
     survey_complete = plan.daily_readiness_survey_completed()
     landing_screen, nav_bar_indicator = plan.define_landing_screen()
@@ -53,8 +62,23 @@ def handle_active_recovery_update():
     plan['nav_bar_indicator'] = nav_bar_indicator
     del plan['daily_readiness_survey'], plan['user_id']
 
-
     return {'daily_plans': [plan]}, 202
+
+
+def save_completed_exercises_for_recovery(recovery_session, user_id, event_date):
+    save_completed_exercises(recovery_session.inhibit_exercises, user_id, event_date)
+    save_completed_exercises(recovery_session.lengthen_exercises, user_id, event_date)
+    save_completed_exercises(recovery_session.activate_exercises, user_id, event_date)
+    save_completed_exercises(recovery_session.integrate_exercises, user_id, event_date)
+
+
+def save_completed_exercises(exercise_list, user_id, event_date):
+    exercise_store = CompletedExerciseDatastore()
+
+    for exercise in exercise_list:
+        exercise_store.put(CompletedExercise(athlete_id=user_id,
+                                             exercise_id=exercise.library_id,
+                                             event_date=event_date))
 
 
 def _check_plan_exists(user_id, event_date):
