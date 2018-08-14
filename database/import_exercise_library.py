497286cd23469acbd5b600f6fffc30deeacc0e1c
DIpesh Gautam <dipesh@biometrixtech.com>
2018-08-14 17:17:59 -0400

    update structure of exercise_library

--- database/import_exercise_library.py
+++ database/import_exercise_library.py
@@ -20,60 +20,67 @@ with open('Exercise_Library.csv', newline='') as csvfile:
     row_count = 0
     for row in exercise_reader:
         if row_count > 0:
-            if row[14] != "x":
+            if row[8] != "x" and row[26] != "x":
                 exercise_item = models.exercise.Exercise(row[0])
-                print(row[0])
                 exercise_item.display_name = row[1]
                 exercise_item.name = row[2]
-                if row[16] == "-" or row[16] == "":
+                if row[10] in ["-", ""]:
                     exercise_item.min_sets = 0
                 else:
-                    exercise_item.min_sets = int(row[16])
-                if row[17] == "-" or row[17] == "":
+                    exercise_item.min_sets = int(row[10])
+                if row[11] in ["-", ""]:
                     exercise_item.max_sets = 0
                 else:
-                    exercise_item.max_sets = int(row[17])
-                if row[18] == "-" or row[18] == "":
+                    exercise_item.max_sets = int(row[11])
+                if row[12] in ["-", ""]:
                     exercise_item.min_reps = None
                 else:
-                    exercise_item.min_reps = int(row[18])
-                if row[19] == "-" or row[19] == "":
+                    exercise_item.min_reps = int(row[12])
+                if row[13] in ["-", ""]:
                     exercise_item.max_reps = None
                 else:
-                    exercise_item.max_reps = int(row[19])
-                exercise_item.bilateral = (row[20] == "Y")
-                if row[21] == "-" or row[21] == "":
+                    exercise_item.max_reps = int(row[13])
+                exercise_item.bilateral = (row[14] == "Y")
+                if row[15] in ["-", ""]:
                     exercise_item.progression_interval = 0
                 else:
-                    exercise_item.progression_interval = int(row[21])
-                if row[22] == "-" or row[22] == "":
+                    exercise_item.progression_interval = int(row[15])
+                if row[16] in ["-", ""]:
                     exercise_item.exposure_target = 0
                 else:
-                    exercise_item.exposure_target = int(row[22])
-                exercise_item.unit_of_measure = row[23]
-                if row[24] == "-" or row[24] == "":
+                    exercise_item.exposure_target = int(row[16])
+                if row[17] in ["-", ""]:
+                    exercise_item.minimum_exposure_stage = 0
+                else:
+                    exercise_item.minimum_exposure_stage = int(row[17])
+                exercise_item.unit_of_measure = row[18]
+                if row[19] in ["-", ""]:
                     exercise_item.seconds_rest_between_sets = 0
                 else:
-                    exercise_item.seconds_rest_between_sets = int(row[24])
-                if row[25] == "-" or row[25] == "":
+                    exercise_item.seconds_rest_between_sets = int(row[19])
+                if row[20] in ["-", ""]:
                     exercise_item.seconds_per_set = None
                 else:
-                    exercise_item.seconds_per_set = int(row[25])
-                if row[26] == "-" or row[26] == "":
+                    exercise_item.seconds_per_set = int(row[20])
+                if row[21] in ["-", ""]:
                     exercise_item.seconds_per_rep = None
                 else:
-                    exercise_item.seconds_per_rep = int(row[26])
-                exercise_item.progresses_to = row[27]
-                exercise_item.technical_difficulty = row[29]
-                exercise_item.equipment_required = row[30]
-                exercise_item.youtube_id = row[32]
-                exercise_item.description = exercise_descriptions[exercise_item.id]
+                    exercise_item.seconds_per_rep = int(row[21])
+                exercise_item.progresses_to = row[22]
+                exercise_item.technical_difficulty = row[24]
+                exercise_item.equipment_required = row[25]
+                print(exercise_item.equipment_required)
+                exercise_item.youtube_id = None
+                try:
+                    exercise_item.description = exercise_descriptions[exercise_item.id]
+                except KeyError:
+                    exercise_item.description = ""
                 exercises.append(exercise_item)
         row_count = row_count + 1
 
+import sys
+sys.exit()
 exercise_count = len(exercises)
-# db = get_mongo_database()
-# collection = db.exerciseLibrary_1
 collection = get_mongo_collection('exerciselibrary')
 for exercise_item in exercises:
     collection.replace_one({'library_id': exercise_item.id},
@@ -89,6 +96,7 @@ for exercise_item in exercises:
                            'bilateral': exercise_item.bilateral,
                            'progression_interval': exercise_item.progression_interval,
                            'exposure_target': exercise_item.exposure_target,
+                           'minimum_exposure_stage': exercise_item.minimum_exposure_stage,
                            'unit_of_measure': exercise_item.unit_of_measure,
                            'seconds_rest_between_sets': exercise_item.seconds_rest_between_sets,
                            'time_per_set': exercise_item.seconds_per_set,
