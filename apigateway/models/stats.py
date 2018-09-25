f42d9b9d570bbafaa349d6438d9e29c1ea95bc90
Paul LaForge <paul@biometrixtech.com>
2018-09-25 14:29:28 -0600

    updated logic to new reqs

--- apigateway/models/stats.py
+++ apigateway/models/stats.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from serialisable import Serialisable
-from models.sport import SportName, NoSportPosition, BaseballPosition, BasketballPosition, FootballPosition, LacrossePosition, SoccerPosition, RunningPosition
+from models.sport import SportName, NoSportPosition, BaseballPosition, BasketballPosition, FootballPosition, LacrossePosition, SoccerPosition
 
 
 class FitFatigueStatus(Enum):
@@ -112,8 +112,6 @@ class AthleteStats(Serialisable):
                 value = FootballPosition(value)
             elif self.current_sport_name == SportName.lacrosse:
                 value = LacrossePosition(value)
-            elif self.current_sport_name == SportName.running:
-                value = RunningPosition(value)
         super().__setattr__(name, value)
 
     def json_serialise(self):
