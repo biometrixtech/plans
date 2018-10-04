02d2f46239687d52cb14bd0b8f64a5a3cb85cb79
DIpesh Gautam <dipesh@biometrixtech.com>
2018-10-04 15:06:23 -0400

    schedule for 5 mins not 30

--- apigateway/routes/athlete.py
+++ apigateway/routes/athlete.py
@@ -133,7 +133,7 @@ def manage_recovery_push_notification(athlete_id):
 @require.authenticated.service
 @xray_recorder.capture('routes.athlete.completion_pn')
 def schedule_prep_completion_push_notification(athlete_id):
-    execute_at = datetime.datetime.now() + datetime.timedelta(minutes=30)
+    execute_at = datetime.datetime.now() + datetime.timedelta(minutes=5)
     # execute_at = format_datetime(execute_at)
     body = {"recovery_type": "prep",
             "event_date": format_date(parse_datetime(request.json["event_date"]))}
