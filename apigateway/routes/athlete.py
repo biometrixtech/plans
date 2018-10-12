29c4ff6dd940cb83393cf57f7fba1191765db4f6
DIpesh Gautam <dipesh@biometrixtech.com>
2018-10-12 10:35:15 -0400

    should be scheduled for 30 mins not 5

--- apigateway/routes/athlete.py
+++ apigateway/routes/athlete.py
@@ -137,9 +137,8 @@ def manage_recovery_push_notification(athlete_id):
 @require.authenticated.service
 @xray_recorder.capture('routes.athlete.completion_pn')
 def schedule_prep_completion_push_notification(athlete_id):
-    execute_at = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    execute_at = datetime.datetime.now() + datetime.timedelta(minutes=30)
     print(f"scheduled prep notification at {execute_at}")
-    # execute_at = format_datetime(execute_at)
     body = {"recovery_type": "prep",
             "event_date": format_date(parse_datetime(request.json["event_date"]))}
     plans_service = Service('plans', Config.get('API_VERSION'))
