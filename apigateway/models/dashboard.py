f8b567147e0eaf0be25b3350781e6d1a5c46e551
DIpesh Gautam <dipesh@biometrixtech.com>
2018-11-28 10:03:44 -0500

    API aggregation cleanup

--- apigateway/models/dashboard.py
+++ apigateway/models/dashboard.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from serialisable import Serialisable
 from models.metrics import MetricColor
 
@@ -9,17 +8,17 @@ class TeamDashboardData(Serialisable):
         self.name = name
         self.compliance = []
         self.daily_insights_dict = {}
-        self.daily_insights = []
+        self.daily_insights = DailySummary()
         self.weekly_insights_dict = {}
-        self.weekly_insights = []
+        self.weekly_insights = WeeklySummary()
         self.athletes = []
 
 
     def json_serialise(self):
         ret = {'name': self.name,
                'compliance': self.compliance,
-               'daily_insights': [d.json_serialise() for d in self.daily_insights],
-               'weekly_insights': [w.json_serialise() for w in self.weekly_insights],
+               'daily_summary': self.daily_insights.json_serialise(),
+               'weekly_insights': self.weekly_insights.json_serialise(),
                'athletes': [a.json_serialise() for a in self.athletes]
                 }
         return ret
@@ -50,13 +49,50 @@ class TeamDashboardData(Serialisable):
             athlete.cleared_to_train = False if athlete.color.value == 2 else True
 
 
+class DailySummary(Serialisable):
+    def __init__(self):
+        self.all_good = []
+        self.increase_workload = []
+        self.limit_time_intensity_of_training = []
+        self.monitor_in_training = []
+        self.not_cleared_for_training = []
+
+
+    def json_serialise(self):
+        ret = {'all_good': [a.json_serialise() for a in self.all_good],
+               'increase_workload': [a.json_serialise() for a in self.increase_workload],
+               'limit_time_intensity_of_training': [a.json_serialise() for a in self.limit_time_intensity_of_training],
+               'monitor_in_training': [a.json_serialise() for a in self.monitor_in_training],
+               'not_cleared_for_training': [a.json_serialise() for a in self.not_cleared_for_training]
+                }
+        return ret
+
+class WeeklySummary(Serialisable):
+    def __init__(self):
+        self.all_good = []
+        self.balance_overtraining_risk = []
+        self.add_variety_to_training_risk = []
+        self.increase_weekly_workload = []
+        self.address_pain_or_soreness = []
+        self.evaluate_health_status = []
+
+    def json_serialise(self):
+        ret = {'all_good': [a.json_serialise() for a in self.all_good],
+               'balance_overtraining_risk': [a.json_serialise() for a in self.balance_overtraining_risk],
+               'add_variety_to_training_risk': [a.json_serialise() for a in self.add_variety_to_training_risk],
+               'increase_weekly_workload': [a.json_serialise() for a in self.increase_weekly_workload],
+               'address_pain_or_soreness': [a.json_serialise() for a in self.address_pain_or_soreness],
+               'evaluate_health_status': [a.json_serialise() for a in self.evaluate_health_status]
+                }
+        return ret
+
 class AthleteDashboardSummary(Serialisable):
     def __init__(self, user_id, first_name, last_name):
         self.user_id = user_id
         self.first_name = first_name
         self.last_name = last_name
         self.cleared_to_train = True
-        self.color = 0
+        self.color = MetricColor(0)
 
     def json_serialise(self):
         ret = {'user_id': self.user_id,
