cc6f547f64cb4f4cdccd70b7567e83b43b75de6c
DIpesh Gautam <dipesh@biometrixtech.com>
2018-11-28 13:06:19 -0500

    get recommendation text

--- apigateway/models/metrics.py
+++ apigateway/models/metrics.py
@@ -66,7 +66,7 @@ class AthleteTrainingVolumeMetricGenerator(object):
                 metric.high_level_action_description = self.thresholds[key].high_level_action_description
                 metric.specific_insight_training_volume = self.thresholds[key].specific_insight_training_volume
                 metric.high_level_insight = self.thresholds[key].high_level_insight
-                metric.specific_actions = self.thresholds[key].specific_actions
+                metric.specific_actions = [TextGenerator().get_recommendation_text(rec=rec) for rec in self.thresholds[key].specific_actions]
                 metric.color = self.thresholds[key].color
                 metric_list.append(metric)
 
@@ -95,34 +95,6 @@ class AthleteSorenessMetricGenerator(object):
                     if v.high_value >= getattr(s, self.threshold_attribute):
                         v.soreness_list.append(s)
 
-    def get_body_part_text(self, text, soreness_list):
-
-        body_part_list = []
-        for soreness in soreness_list:
-            try:
-                part = BodyPartLocationText(soreness.body_part.location).value()
-            except:
-                part = BodyPartLocationText(soreness.body_part_location).value()
-            side = soreness.side
-            if side == 1:
-                body_text = ' '.join(['left', part])
-            elif side == 2:
-                body_text = ' '.join(['right', part])
-            else:  # side == 0:
-                body_text = part
-
-            body_part_list.append(body_text)
-        if len(body_part_list) > 1:
-            joined_text = ', '.join(body_part_list)
-            pos = joined_text.rfind(',')
-            joined_text = joined_text[:pos] + ' and' + joined_text[pos+1:]
-            return text.format(bodypart=joined_text)
-        elif len(body_part_list) == 1:
-            joined_text = body_part_list[0]
-            return text.format(bodypart=joined_text)
-        else:
-            return text
-
     def get_metric_list(self):
 
         metric_list = []
@@ -131,10 +103,10 @@ class AthleteSorenessMetricGenerator(object):
               if len(self.thresholds[key].soreness_list) > 0:
                   metric = AthleteMetric(self.name, self.metric_type)
                   metric.high_level_action_description = self.thresholds[key].high_level_action_description
-                  metric.specific_insight_recovery = self.get_body_part_text(self.thresholds[key].specific_insight_recovery,
-                                                                             self.thresholds[key].soreness_list)
+                  metric.specific_insight_recovery = TextGenerator().get_body_part_text(self.thresholds[key].specific_insight_recovery,
+                                                                                        self.thresholds[key].soreness_list)
                   metric.high_level_insight = self.thresholds[key].high_level_insight
-                  metric.specific_actions = self.thresholds[key].specific_actions
+                  metric.specific_actions = [TextGenerator().get_recommendation_text(rec=rec, soreness=self.thresholds[key].soreness_list) for rec in self.thresholds[key].specific_actions]
                   metric.color = self.thresholds[key].color
                   metric_list.append(metric)
 
@@ -184,3 +156,77 @@ class ThresholdRecommendation(object):
         self.high_value = high_value
         self.soreness_list = []
         self.count = 0
+
+
+class TextGenerator(object):
+    def get_recommendation_text(self, rec, soreness=None):
+        text = RecommendationText(rec).value()
+        if soreness is None:
+            return text
+        else:
+            text = self.get_body_part_text(text, soreness)
+            try:
+                is_pain = soreness[0].pain
+            except:
+                is_pain = soreness[0].is_pain
+            if is_pain:
+                return text.format(is_pain="pain")
+            else:
+                return text.format(is_pain="soreness")
+
+    def get_body_part_text(self, text, soreness_list=[]):
+
+        body_part_list = []
+        for soreness in soreness_list:
+            try:
+                part = BodyPartLocationText(soreness.body_part.location).value()
+            except:
+                part = BodyPartLocationText(soreness.body_part_location).value()
+            side = soreness.side
+            if side == 1:
+                body_text = " ".join(["left", part])
+            elif side == 2:
+                body_text = " ".join(["right", part])
+            else:  # side == 0:
+                body_text = part
+
+            body_part_list.append(body_text)
+        if len(body_part_list) > 1:
+            joined_text = ", ".join(body_part_list)
+            pos = joined_text.rfind(",")
+            joined_text = joined_text[:pos] + " and" + joined_text[pos+1:]
+            return text.format(bodypart=joined_text, is_pain="pain")
+        elif len(body_part_list) == 1:
+            joined_text = body_part_list[0]
+            return text.format(bodypart=joined_text, is_pain="pain")
+        else:
+            return text
+
+class RecommendationText(object):
+    def __init__(self, rec):
+        self.rec = rec
+
+    def value(self):
+        recs = {"1A": "Creating a periodized plan for the next month to introduce a variety of different stimuli",
+                "1B": "Increase variety in your training regimen",
+                "2A": "Considering not training today",
+                "2B": "A shorter or lower-intensity training session",
+                "2C": "Exposure to a longer or higher-intensity training session",
+                "3A": "Drastically decreasing workload this week",
+                "3B": "Consider decreasing weekly workload",
+                "3C": "Consider increasing weekly workload",
+                "5A": "Seek help from a medical professional",
+                "5B": "Limiting your training.",
+                "5C": "Seek medical advice if symptoms persist after warm-up.", 
+                "6A": "Stop activity if {bodypart} {is_pain} are present",
+                "6B": "Modifying training so all activity is free of {bodypart} {is_pain}",
+                "6C": "Monitor {body_part} symptoms during training. If symptoms persists, consider decreasing training in the next few days to allow recovery",
+                "7A": "Completing Fathom's personalized Prep & Recovery",
+                "7B": "Complete Prep",
+                "8A": "Heat {bodypart} before training for 10 minutes",
+                "9A": "Ice {bodypart} immediately after training for 20 minutes",
+                "9C": "Ice bath for 10 minutes after training"
+                }
+
+        return recs[self.rec]
+
