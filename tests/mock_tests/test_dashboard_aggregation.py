1f5c8b032864438cc59fdf926cbfcc9404d5a0b9
DIpesh Gautam <dipesh@biometrixtech.com>
2018-11-30 12:50:44 -0500

    compliance and team grouping tests

--- tests/mock_tests/test_dashboard_aggregation.py
+++ tests/mock_tests/test_dashboard_aggregation.py
@@ -1,10 +1,7 @@
-from logic.metrics_processing import MetricsProcessing
-from models.stats import AthleteStats
 from models.metrics import AthleteMetric, MetricColor, WeeklyHighLevelInsight, DailyHighLevelInsight, MetricType, SpecificAction
-from models.soreness import BodyPartLocation, HistoricSorenessStatus, HistoricSoreness
-from models.dashboard import AthleteDashboardData
-
-
+from models.dashboard import AthleteDashboardData, TeamDashboardData
+from models.daily_readiness import DailyReadiness
+import datetime
 
 def get_metric(metric_type, color, high_level_insight, specific_insight, rec1, rec2):
     metric = AthleteMetric('Metric', metric_type)
@@ -14,6 +11,11 @@ def get_metric(metric_type, color, high_level_insight, specific_insight, rec1, r
     metric.specific_actions = [rec1, rec2]
     return metric
 
+
+def get_athlete(metrics):
+    athlete = AthleteDashboardData("user_id", "fisrt_name", 'last_name')
+    athlete.aggregate(metrics)
+    return athlete
 
 def test_no_metrics():
     metrics = []
@@ -138,3 +140,92 @@ def test_cleared_to_train():
     assert athlete.weekly_recommendation == set(["metric1_rec1", "metric2_rec1"])
     assert athlete.daily_recommendation == set(["metric1_rec2", "metric2_rec2"])
     assert athlete.insights == ["Metric1 insight", "Metric2 insight"]
+
+
+def test_athlete_grouping():
+    daily_increase_workload = get_metric(MetricType.daily,
+                                         MetricColor.yellow,
+                                         DailyHighLevelInsight.increase_workload,
+                                         "insight",
+                                         SpecificAction("1B", "rec1", True),
+                                         SpecificAction("2B", "rec2", True)
+                                         )
+    daily_limit_time_intensity_of_training = get_metric(MetricType.daily,
+                                                        MetricColor.yellow,
+                                                        DailyHighLevelInsight.limit_time_intensity_of_training,
+                                                        "insight",
+                                                        SpecificAction("1B", "rec1", True),
+                                                        SpecificAction("2B", "rec2", True)
+                                                        )
+    daily_monitor_in_training = get_metric(MetricType.daily,
+                                           MetricColor.yellow,
+                                           DailyHighLevelInsight.monitor_in_training,
+                                           "insight",
+                                           SpecificAction("1B", "rec1", True),
+                                           SpecificAction("2B", "rec2", True)
+                                           )
+
+    weekly_balance_overtraining_risk = get_metric(MetricType.longitudinal,
+                                                  MetricColor.yellow,
+                                                  WeeklyHighLevelInsight.balance_overtraining_risk,
+                                                  "insight",
+                                                  SpecificAction("1B", "rec1", True),
+                                                  SpecificAction("2B", "rec2", True)
+                                                 )
+    weekly_add_variety_to_training_risk = get_metric(MetricType.longitudinal,
+                                                     MetricColor.yellow,
+                                                     WeeklyHighLevelInsight.add_variety_to_training_risk,
+                                                     "insight",
+                                                     SpecificAction("1B", "rec1", True),
+                                                     SpecificAction("2B", "rec2", True)
+                                                    )
+
+    weekly_evaluate_health_status = get_metric(MetricType.longitudinal,
+                                               MetricColor.red,
+                                               WeeklyHighLevelInsight.evaluate_health_status,
+                                               "insight",
+                                               SpecificAction("1B", "rec1", True),
+                                               SpecificAction("2B", "rec2", True)
+                                              )
+    team = TeamDashboardData("Fathom Team")
+    athlete1 = get_athlete([daily_limit_time_intensity_of_training, daily_monitor_in_training, weekly_balance_overtraining_risk])
+    team.insert_user(athlete1)
+    athlete2 = get_athlete([daily_increase_workload, weekly_add_variety_to_training_risk])
+    team.insert_user(athlete2)
+    athlete3 = get_athlete([daily_limit_time_intensity_of_training, daily_monitor_in_training, weekly_add_variety_to_training_risk])
+    team.insert_user(athlete3)
+    athlete4 = get_athlete([daily_limit_time_intensity_of_training, daily_monitor_in_training, weekly_evaluate_health_status])
+    team.insert_user(athlete4)
+
+
+    assert len(team.daily_insights.all_good) == 0
+    assert len(team.daily_insights.increase_workload) == 1
+    assert len(team.daily_insights.limit_time_intensity_of_training) == 2
+    assert len(team.daily_insights.monitor_in_training) == 2
+    assert len(team.daily_insights.not_cleared_for_training) == 1
+
+    assert len(team.weekly_insights.all_good) == 0
+    assert len(team.weekly_insights.balance_overtraining_risk) == 1
+    assert len(team.weekly_insights.add_variety_to_training_risk) == 2
+    assert len(team.weekly_insights.increase_weekly_workload) == 0
+    assert len(team.weekly_insights.address_pain_or_soreness) == 0
+    assert len(team.weekly_insights.evaluate_health_status) == 1
+
+
+def test_compliance_grouping():
+    user_ids = ["1", "2", "3", "4", "5"]
+    users = {}
+    for user_id in user_ids:
+        users[user_id] = {"user_id": user_id,
+                          "first_name": "user"+user_id,
+                          "last_name": "last_name"
+                          }
+    readiness_survey_list = [DailyReadiness("2018-11-30T10:00:00Z", "1", [], 5, 7),
+                             DailyReadiness("2018-11-30T10:00:00Z", "2", [], 5, 7),
+                             DailyReadiness("2018-11-30T10:00:00Z", "3", [], 5, 7)]
+    team = TeamDashboardData("Fathom Team")
+    team.get_compliance_data(user_ids, users, readiness_survey_list)
+
+
+    assert len(team.compliance["completed"]) == 3
+    assert len(team.compliance["incomplete"]) == 2
