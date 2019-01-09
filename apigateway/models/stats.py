a4046a8eb26680d9cd31c3248f99f8b7c13577cb
DIpesh Gautam <dipesh@biometrixtech.com>
2019-01-09 13:36:55 -0500

    not getting q2 correctly

--- apigateway/models/stats.py
+++ apigateway/models/stats.py
@@ -225,13 +225,15 @@ class AthleteStats(Serialisable):
                              "side": soreness.side,
                              "pain": soreness.is_pain,
                              "status": soreness.historic_soreness_status.name}
-                if len(q2) > 0:
+                if {new_part["body_part"]: new_part["side"]} in unique_q2:
                     for q2_part in q2:
-                        if q2_part["body_part"] == new_part["body_part"] and q2_part["side"] == new_part["side"]:
-                            q2_part["pain"] = True
-                        else:
-                            q2.append(new_part)
+                        if q2_part['new_part'] == new_part['body_part'] and q2_part['side'] == new_part['side']:
+                            if new_part['pain']:
+                                q2_part['pain'] = True
+                                q2_part['status'] = new_part['status']
+                            break
                 else:
+                    unique_q2.append({new_part["body_part"]: new_part["side"]})
                     q2.append(new_part)
 
         return q2, q3, tipping_status
