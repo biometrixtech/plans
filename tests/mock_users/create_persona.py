333462d7d62ebf8c92ee441fbcb7a0b8949d7750
DIpesh Gautam <dipesh@biometrixtech.com>
2019-03-01 11:55:37 -0500

    use login to get user_id in creating persona

--- tests/mock_users/create_persona.py
+++ tests/mock_users/create_persona.py
@@ -1,12 +1,21 @@
 import os
 import time
+import json
 from aws_xray_sdk.core import xray_recorder
-os.environ['ENVIRONMENT'] = 'dev'
+os.environ['ENVIRONMENT'] = 'test'
 xray_recorder.configure(sampling=False)
 xray_recorder.begin_segment(name="test")
+import requests
 
 from persona import Persona
 from soreness_history import create_body_part_history, persistent2_question, acute_pain_no_question
+
+def login_user(email):
+    body = {"password": "Fathom123!", "personal_data": {"email": email}}
+    headers = {"Content-Type": "application/json"}
+    url = "http://apis.{env}.fathomai.com/users/2_1/user/login".format(env=os.environ['ENVIRONMENT'])
+    response = requests.post(url, data=json.dumps(body), headers=headers)
+    return response.json()['user']['id']
 
 def create_persona_with_two_historic_soreness(days):
     soreness_history = []
@@ -14,7 +23,9 @@ def create_persona_with_two_historic_soreness(days):
     soreness_history.append(right_knee_persistent2_question)
     left_knee_acute_pain_no_question = create_body_part_history(acute_pain_no_question(), 7, 1, True)
     soreness_history.append(left_knee_acute_pain_no_question)
-    persona1 = Persona('d2ad1be7-18ba-4126-9fc3-047c37a60018')
+    user_id = login_user("dipesh+persona1@fathomai.com")
+    print(user_id)
+    persona1 = Persona(user_id)
     persona1.soreness_history = soreness_history
     persona1.create_history(days=days)
 
