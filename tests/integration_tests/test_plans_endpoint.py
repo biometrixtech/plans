55166020dab38922ded98e534fc16522826fcfff
DIpesh Gautam <dipesh@biometrixtech.com>
2019-03-29 09:40:42 -0400

    reorder tests

--- tests/integration_tests/test_plans_endpoint.py
+++ tests/integration_tests/test_plans_endpoint.py
@@ -42,6 +42,39 @@ def test_get_plan_one_plan_readiness_completed():
     assert len(readiness) == 0
 
 
+# 4 days ago
+def test_change_active_time_start_and_complete_recovery():
+    if utils.HEADERS['Authorization'] is None:
+        utils.login_user(utils.USER['email'])
+        utils.reset_user()
+    event_date = datetime.datetime.now() - datetime.timedelta(days=4)
+    existing_active_time = utils.get_plan(format_date(event_date), format_datetime(event_date)).json()['daily_plans'][0]['post_recovery']['minutes_duration']
+
+    active_time = 15
+    if existing_active_time in [15, 0]:
+        active_time = 20
+    response = utils.change_active_time(event_date, active_time)
+    assert response.status_code == 200
+    plan = response.json()['daily_plans'][0]
+
+    assert plan['post_recovery']['minutes_duration'] == active_time
+    assert plan['landing_screen'] == 2
+
+    response2 = utils.start_recovery(event_date + datetime.timedelta(minutes=2), recovery_type='post')
+    assert response2.status_code == 200
+
+    response3 = utils.complete_recovery(event_date + datetime.timedelta(minutes=12),
+                                        completed_exercises=[plan["post_recovery"]["inhibit_exercises"][0]['library_id']],
+                                        recovery_type='post')
+    assert response3.status_code == 202
+    plan = response3.json()["daily_plans"][0]
+    assert plan["post_recovery_completed"]
+    assert plan["post_recovery"]["completed"]
+    assert plan["landing_screen"] == 2
+    assert not plan["post_recovery"]["display_exercises"]
+
+
+# two days ago
 def test_submit_one_session():
     if utils.HEADERS['Authorization'] is None:
         utils.login_user(utils.USER['email'])
@@ -61,6 +94,7 @@ def test_submit_one_session():
     assert plan['post_recovery']['minutes_duration'] == 15
 
 
+# today
 def test_submit_readiness_one_soreness():
     if utils.HEADERS['Authorization'] is None:
         utils.login_user(utils.USER['email'])
@@ -80,26 +114,7 @@ def test_submit_readiness_one_soreness():
     assert plan['landing_screen'] == 0
 
 
-def test_submit_readiness_max_pain_no_prep():
-    if utils.HEADERS['Authorization'] is None:
-        utils.login_user(utils.USER['email'])
-        utils.reset_user()
-    event_date = datetime.datetime.now() + datetime.timedelta(days=2)
-    soreness = [utils.get_soreness(body_part=14, side=1, pain=True, severity=3, movement=5)]
-    response = utils.submit_readiness(event_date,
-                                      soreness=soreness,
-                                      clear_candidates=[],
-                                      sessions=[])
-
-    assert response.status_code == 201
-    plan = response.json()['daily_plans'][0]
-
-    assert plan['daily_readiness_survey_completed']
-    assert plan['pre_recovery']['minutes_duration'] == 15
-    assert len(plan['pre_recovery']['inhibit_exercises']) == len(plan['pre_recovery']['lengthen_exercises']) == len(plan['pre_recovery']['activate_exercises']) == 0
-    assert plan['landing_screen'] == 0
-
-
+# one day later
 def test_submit_readiness_answer_clear_candidates_clear():
     if utils.HEADERS['Authorization'] is None:
         utils.login_user(utils.USER['email'])
@@ -138,6 +153,7 @@ def test_submit_readiness_answer_clear_candidates_clear():
     utils.reset_user()
 
 
+# one day later
 def test_submit_readiness_answer_clear_candidates_not_clear():
     if utils.HEADERS['Authorization'] is None:
         utils.login_user(utils.USER['email'])
@@ -176,32 +192,22 @@ def test_submit_readiness_answer_clear_candidates_not_clear():
     utils.reset_user()
 
 
-def test_change_active_time_start_and_complete_recovery():
+# two days later
+def test_submit_readiness_max_pain_no_prep():
     if utils.HEADERS['Authorization'] is None:
         utils.login_user(utils.USER['email'])
         utils.reset_user()
-    event_date = datetime.datetime.now() - datetime.timedelta(days=4)
-    existing_active_time = utils.get_plan(format_date(event_date), format_datetime(event_date)).json()['daily_plans'][0]['post_recovery']['minutes_duration']
+    event_date = datetime.datetime.now() + datetime.timedelta(days=2)
+    soreness = [utils.get_soreness(body_part=14, side=1, pain=True, severity=3, movement=5)]
+    response = utils.submit_readiness(event_date,
+                                      soreness=soreness,
+                                      clear_candidates=[],
+                                      sessions=[])
 
-    active_time = 15
-    if existing_active_time in [15, 0]:
-        active_time = 20
-    response = utils.change_active_time(event_date, active_time)
-    assert response.status_code == 200
+    assert response.status_code == 201
     plan = response.json()['daily_plans'][0]
 
-    assert plan['post_recovery']['minutes_duration'] == active_time
-    assert plan['landing_screen'] == 2
-
-    response2 = utils.start_recovery(event_date + datetime.timedelta(minutes=2), recovery_type='post')
-    assert response2.status_code == 200
-
-    response3 = utils.complete_recovery(event_date + datetime.timedelta(minutes=12),
-                                        completed_exercises=[plan["post_recovery"]["inhibit_exercises"][0]['library_id']],
-                                        recovery_type='post')
-    assert response3.status_code == 202
-    plan = response3.json()["daily_plans"][0]
-    assert plan["post_recovery_completed"]
-    assert plan["post_recovery"]["completed"]
-    assert plan["landing_screen"] == 2
-    assert not plan["post_recovery"]["display_exercises"]
+    assert plan['daily_readiness_survey_completed']
+    assert plan['pre_recovery']['minutes_duration'] == 15
+    assert len(plan['pre_recovery']['inhibit_exercises']) == len(plan['pre_recovery']['lengthen_exercises']) == len(plan['pre_recovery']['activate_exercises']) == 0
+    assert plan['landing_screen'] == 0
