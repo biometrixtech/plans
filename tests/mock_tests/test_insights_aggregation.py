4542ee70a5ae0279dd3d56f983649bb5f7972f22
DIpesh Gautam <dipesh@biometrixtech.com>
2019-05-15 17:07:49 -0400

    formatting

--- tests/mock_tests/test_insights_aggregation.py
+++ tests/mock_tests/test_insights_aggregation.py
@@ -17,7 +17,7 @@ def test_insights_text():
     assert insight.title == "Mitigate Overuse Injury"
 
 
-def test_aggregate_alerts():
+def test_aggregate_alerts_first_exposure():
     goal = AthleteGoal('Care for Pain', 0, AthleteGoalType(0))
     goal.trigger_type = TriggerType(14)
     alert1 = Alert(goal)
@@ -30,8 +30,8 @@ def test_aggregate_alerts():
     alerts = [alert1, alert2]
     insights = AlertsProcessing.aggregate_alerts(datetime.datetime.now(), alerts, exposed_triggers, [])
     assert len(insights) == 1
-    assert insights[0].first == True
-    assert insights[0].parent == False
+    assert insights[0].first
+    assert not insights[0].parent
     assert insights[0].insight_type == InsightType.daily
 
 
@@ -44,8 +44,8 @@ def test_aggregate_alerts_exposed():
     alerts = [alert1]
     insights = AlertsProcessing.aggregate_alerts(datetime.datetime.now(), alerts, [TriggerType(14)], [])
     assert len(insights) == 1
-    assert insights[0].first == False
-    assert insights[0].parent == False
+    assert not insights[0].first
+    assert not insights[0].parent
     assert insights[0].insight_type == InsightType.daily
 
 
@@ -58,9 +58,10 @@ def test_aggregate_alerts_exposed_group():
     alerts = [alert1]
     insights = AlertsProcessing.aggregate_alerts(datetime.datetime.now(), alerts, [TriggerType(7)], [])
     assert len(insights) == 1
-    assert insights[0].first == False
-    assert insights[0].parent == False
+    assert not insights[0].first
+    assert not insights[0].parent
     assert insights[0].insight_type == InsightType.longitudinal
+
 
 def test_aggregate_alerts_longitutinal():
     current_date_time = datetime.datetime.now()
@@ -72,15 +73,15 @@ def test_aggregate_alerts_longitutinal():
     alerts = [alert1]
     insights = AlertsProcessing.aggregate_alerts(current_date_time - datetime.timedelta(days=5), alerts, [], [])
     assert len(insights) == 1
-    assert insights[0].first == True
-    assert insights[0].parent == False
+    assert insights[0].first
+    assert not insights[0].parent
     assert insights[0].insight_type == InsightType.longitudinal
     assert insights[0].start_date_time == current_date_time - datetime.timedelta(days=5)
 
     insights = AlertsProcessing.aggregate_alerts(current_date_time, alerts, [TriggerType(6)], insights)
     assert len(insights) == 1
-    assert insights[0].first == False
-    assert insights[0].parent == False
+    assert not insights[0].first
+    assert not insights[0].parent
     assert insights[0].insight_type == InsightType.longitudinal
     assert insights[0].start_date_time == current_date_time - datetime.timedelta(days=5)
 
@@ -95,15 +96,15 @@ def test_aggregate_alerts_cleared():
     alerts = [alert1]
     existing_insights = AlertsProcessing.aggregate_alerts(current_date_time - datetime.timedelta(days=5), alerts, [], [])
     assert len(existing_insights) == 1
-    assert existing_insights[0].first == True
-    assert existing_insights[0].parent == False
+    assert existing_insights[0].first
+    assert not existing_insights[0].parent
     assert existing_insights[0].insight_type == InsightType.longitudinal
     assert existing_insights[0].start_date_time == current_date_time - datetime.timedelta(days=5)
 
     insights = AlertsProcessing.aggregate_alerts(current_date_time, [], [TriggerType(6)], existing_insights)
     assert len(insights) == 1
-    assert insights[0].first == True
-    assert insights[0].parent == False
+    assert insights[0].first
+    assert not insights[0].parent
     assert insights[0].insight_type == InsightType.longitudinal
-    assert insights[0].cleared == True
+    assert insights[0].cleared
     assert insights[0].start_date_time == current_date_time - datetime.timedelta(days=5)
