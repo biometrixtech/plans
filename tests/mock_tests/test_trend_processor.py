57619084c15efb4354a24e5323c9931fde30b7a4
Paul LaForge <paul@biometrixtech.com>
2019-07-16 15:45:07 -0600

    fixed trigger 19 test

--- tests/mock_tests/test_trend_processor.py
+++ tests/mock_tests/test_trend_processor.py
@@ -1,4 +1,5 @@
 from logic.trend_processing import TrendProcessor
+from logic.trigger_processing import TriggerFactory
 from models.trigger import TriggerType, Trigger
 from models.body_parts import BodyPartFactory
 from models.soreness_base import BodyPartLocation, BodyPartSide
@@ -9,12 +10,13 @@ def test_trigger_19():
 
     trigger_list = []
     body_part_factory = BodyPartFactory()
+    trigger_factory = TriggerFactory(datetime.now(), None, [], [])
 
     trigger = Trigger(TriggerType.hist_sore_greater_30)
     trigger.body_part = BodyPartSide(body_part_location=BodyPartLocation(8), side=1)
     body_part = body_part_factory.get_body_part(trigger.body_part)
-    trigger.synergists = body_part.synergists
-    trigger.antagonists = body_part.antagonists
+    trigger.synergists = trigger_factory.convert_body_part_list(trigger.body_part, body_part.synergists)
+    trigger.antagonists = trigger_factory.convert_body_part_list(trigger.body_part, body_part.antagonists)
     trigger.created_date_time = datetime.now() - timedelta(days=1)
     trigger.modified_date_time = datetime.now()
 
