f6d8cf91f531660b51b16374af6b3780d770e093
DIpesh Gautam <dipesh@biometrixtech.com>
2019-07-29 14:43:28 -0400

    fte

--- apigateway/routes/trends.py
+++ apigateway/routes/trends.py
@@ -15,7 +15,7 @@ app = Blueprint('trends', __name__)
 @require.authenticated.any
 @require.body({'event_date': str})
 @xray_recorder.capture('routes.trends.plan_alerts.clear')
-def handle_insights_read(principal_id):
+def handle_alerts_cleared(principal_id):
     user_id = principal_id
     event_date = parse_datetime(request.json['event_date'])
     plan_event_day = format_date(event_date)
@@ -38,43 +38,43 @@ def handle_insights_read(principal_id):
     return {'message': 'success'}, 200
 
 
-# @app.route('first_time_experience/category', methods=['POST'])
-# @require.authenticated.any
-# @require.body({'event_date': str})
-# @xray_recorder.capture('routes.trends.first_time_experience.category')
-# def handle_insights_read(principal_id):
-#     user_id = principal_id
-#     event_date = parse_datetime(request.json['event_date'])
-#     plan_event_day = format_date(event_date)
-#     plan = DatastoreCollection().daily_plan_datastore.get(user_id, start_date=plan_event_day, end_date=plan_event_day)[0]
-#     athlete_stats = DatastoreCollection().athlete_stats_datastore.get(user_id)
-#     fte_insight_type = InsightType(request.json['insight_type'])
-#     trend_category = [category for category in plan.trends.trend_categories if category.insight_type == fte_insight_type][0]
-#     trend_category.first_time_experience = False
-#     athlete_stats.trend_categories = plan.trends.trend_categories
-#     DatastoreCollection().athlete_stats_datastore.put(athlete_stats)
-#     DatastoreCollection().daily_plan_datastore.put(plan)
-#
-#     return {'message': 'success'}, 200
-#
-#
-# @app.route('first_time_experience/view', methods=['POST'])
-# @require.authenticated.any
-# @require.body({'event_date': str})
-# @xray_recorder.capture('routes.trends.first_time_experience.view')
-# def handle_insights_read(principal_id):
-#     user_id = principal_id
-#     event_date = parse_datetime(request.json['event_date'])
-#     plan_event_day = format_date(event_date)
-#     plan = DatastoreCollection().daily_plan_datastore.get(user_id, start_date=plan_event_day, end_date=plan_event_day)[0]
-#     athlete_stats = DatastoreCollection().athlete_stats_datastore.get(user_id)
-#     fte_insight_type = InsightType(request.json['insight_type'])
-#     fte_view_type = VisualizationType(request.json['visualization_type'])
-#     fte_trend_category = [category for category in plan.trends.trend_categories if category.insight_type == fte_insight_type][0]
-#     fte_view = [trend for trend in fte_trend_category.trends if trend.visualization_type == fte_view_type]
-#     fte_view.first_time_experience = False
-#     athlete_stats.trend_categories = plan.trends.trend_categories
-#     DatastoreCollection().athlete_stats_datastore.put(athlete_stats)
-#     DatastoreCollection().daily_plan_datastore.put(plan)
-#
-#     return {'message': 'success'}, 200
+@app.route('first_time_experience/category', methods=['POST'])
+@require.authenticated.any
+@require.body({'event_date': str})
+@xray_recorder.capture('routes.trends.first_time_experience.category')
+def handle_fte_category(principal_id):
+    user_id = principal_id
+    event_date = parse_datetime(request.json['event_date'])
+    plan_event_day = format_date(event_date)
+    plan = DatastoreCollection().daily_plan_datastore.get(user_id, start_date=plan_event_day, end_date=plan_event_day)[0]
+    athlete_stats = DatastoreCollection().athlete_stats_datastore.get(user_id)
+    fte_insight_type = InsightType(request.json['insight_type'])
+    trend_category = [category for category in plan.trends.trend_categories if category.insight_type == fte_insight_type][0]
+    trend_category.first_time_experience = False
+    athlete_stats.trend_categories = plan.trends.trend_categories
+    DatastoreCollection().athlete_stats_datastore.put(athlete_stats)
+    DatastoreCollection().daily_plan_datastore.put(plan)
+
+    return {'message': 'success'}, 200
+
+
+@app.route('first_time_experience/view', methods=['POST'])
+@require.authenticated.any
+@require.body({'event_date': str})
+@xray_recorder.capture('routes.trends.first_time_experience.view')
+def handle_fte_view(principal_id):
+    user_id = principal_id
+    event_date = parse_datetime(request.json['event_date'])
+    plan_event_day = format_date(event_date)
+    plan = DatastoreCollection().daily_plan_datastore.get(user_id, start_date=plan_event_day, end_date=plan_event_day)[0]
+    athlete_stats = DatastoreCollection().athlete_stats_datastore.get(user_id)
+    fte_insight_type = InsightType(request.json['insight_type'])
+    fte_viz_type = VisualizationType(request.json['visualization_type'])
+    fte_trend_category = [category for category in plan.trends.trend_categories if category.insight_type == fte_insight_type][0]
+    fte_view = [trend for trend in fte_trend_category.trends if trend.visualization_type == fte_viz_type]
+    fte_view.first_time_experience = False
+    athlete_stats.trend_categories = plan.trends.trend_categories
+    DatastoreCollection().athlete_stats_datastore.put(athlete_stats)
+    DatastoreCollection().daily_plan_datastore.put(plan)
+
+    return {'message': 'success'}, 200
