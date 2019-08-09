638e36d0cc65ca4a719c13f6e8e4a772eb506c00
DIpesh Gautam <dipesh@biometrixtech.com>
2019-08-09 17:36:46 -0400

    typo

--- apigateway/logic/training_volume_processing.py
+++ apigateway/logic/training_volume_processing.py
@@ -265,7 +265,7 @@ class TrainingVolumeProcessing(object):
         self.workout_chart = workout_chart
 
         biomechanics_chart = BiomechanicsChart()
-        biomechanics_chart.add_sessions(last_14_day_training_sessions)
+        biomechanics_chart.add_sessions(last_14_day_sessions)
         self.biomechanics_chart = biomechanics_chart
 
         #self.last_week_external_values.extend(
