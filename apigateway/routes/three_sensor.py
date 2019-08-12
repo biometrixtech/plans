53c45d9750a52bfda3a9175308d8d3fa7b94d4a6
DIpesh Gautam <dipesh@biometrixtech.com>
2019-08-12 09:36:23 -0400

    date

--- apigateway/routes/three_sensor.py
+++ apigateway/routes/three_sensor.py
@@ -22,7 +22,7 @@ def handle_biomechanics_detail_get(principal_id=None):
     day_35 = (event_date - datetime.timedelta(days=35)).date()
 
     user_sessions = sorted(datastore.get(user_id=user_id, sessions=7), key=lambda i:i.event_date)
-    user_sessions = [session for session in user_sessions if session.event_date >= day_35]
+    user_sessions = [session for session in user_sessions if session.event_date.date() >= day_35]
     sessions = [s.json_serialise(api=True) for s in user_sessions]
 
     return {'sessions': sessions}, 200
