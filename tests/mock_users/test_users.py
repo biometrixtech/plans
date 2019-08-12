5edd19b1b219aa0be05af2b70c8c710f6abda8f6
Paul LaForge <paul@biometrixtech.com>
2019-08-12 11:49:57 -0600

    add symmetric test user

--- tests/mock_users/test_users.py
+++ tests/mock_users/test_users.py
@@ -18,10 +18,11 @@ users = [
 ]
 
 three_sensor_users = [
-    "tread_a", #6fc48b46-23c8-4490-9885-e109ff63c20e
-    "tread_b", #4673998d-5206-4275-a048-da5dda6a7342
-    "tread_run", #bdb8b194-e748-4197-819b-b356f1fb0629
-    "run_a" #2b4a1792-42c7-460e-9e4c-98627e72cc6f
+    #"tread_a", #6fc48b46-23c8-4490-9885-e109ff63c20e
+    #"tread_b", #4673998d-5206-4275-a048-da5dda6a7342
+    #"tread_run", #bdb8b194-e748-4197-819b-b356f1fb0629
+    #"run_a", #2b4a1792-42c7-460e-9e4c-98627e72cc6f
+    "sym" #7fd0c1d4-61ac-4ce5-9621-16d69501b211
 ]
 
 def get_test_users():
