c40210750ed098734b1b96a0620313cccb8ef73d
Paul LaForge <paul@biometrixtech.com>
2019-09-26 14:59:39 -0600

    Removed delete, put from predictive datastore

--- apigateway/datastores/predictive_datastore.py
+++ apigateway/datastores/predictive_datastore.py
@@ -12,20 +12,6 @@ class PredictiveDatastore(object):
     def get(self):
         return self._query_mongodb()
 
-    def put(self, items):
-        if not isinstance(items, list):
-            items = [items]
-        try:
-            for item in items:
-                self._put_mongodb(item)
-        except Exception as e:
-            raise e
-
-    def delete(self, athlete_id=None):
-        if athlete_id is None:
-            raise InvalidSchemaException("Need to provide athlete_id to delete")
-        self._delete_mongodb(athlete_id=athlete_id)
-
     @xray_recorder.capture('datastore.AthleteStatsDatastore._query_mongodb')
     def _query_mongodb(self):
         mongo_collection = get_mongo_collection(self.mongo_collection)
@@ -39,22 +25,3 @@ class PredictiveDatastore(object):
 
         return athlete_stats_list
 
-
-    @xray_recorder.capture('datastore.AthleteStatsDatastore._put_mongodb')
-    def _put_mongodb(self, item):
-        item = item.json_serialise()
-
-        mongo_collection = get_mongo_collection(self.mongo_collection)
-        query = {'athlete_id': item['athlete_id']}
-        mongo_collection.replace_one(query, item, upsert=True)
-
-    @xray_recorder.capture('datastore.AthleteStatsDatastore._delete_mongodb')
-    def _delete_mongodb(self, athlete_id):
-        mongo_collection = get_mongo_collection(self.mongo_collection)
-        query = {}
-        if isinstance(athlete_id, list):
-            query['athlete_id'] = {'$in': athlete_id}
-        else:
-            query['athlete_id'] = athlete_id
-        if len(query) > 0:
-            mongo_collection.delete_many(query)
