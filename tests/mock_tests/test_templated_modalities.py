f02bcca4490d773cd55a9f00ce9b3c4cec43bf86
Dipesh Gautam <dipesh@biometrixtech.com>
2020-03-19 08:58:18 -0400

    tests

--- tests/mock_tests/test_templated_modalities.py
+++ tests/mock_tests/test_templated_modalities.py
@@ -1,50 +1,54 @@
-# from models.daily_plan import DailyPlan
-# from models.functional_movement_modalities import ModalityType, ActiveRestBeforeTraining, ActiveRestAfterTraining
-# from utils import format_date
-# import datetime
-#
-#
-# def test_on_demand_pre_active_rest_none():
-#     event_date = format_date(datetime.datetime.now())
-#     plan = DailyPlan(event_date)
-#     plan.define_available_modalities()
-#
-#     assert len(plan.modalities_available_on_demand) == 1
-#     assert plan.modalities_available_on_demand[0].type == ModalityType.pre_active_rest
-#
-#
-# def test_on_demand_pre_active_rest_completed():
-#     event_date_time = datetime.datetime.now()
-#     event_date = format_date(event_date_time)
-#     plan = DailyPlan(event_date)
-#     pre_active_rest = ActiveRestBeforeTraining(event_date_time)
-#     pre_active_rest.completed = True
-#     plan.modalities = [pre_active_rest]
-#     plan.define_available_modalities()
-#
-#     assert len(plan.modalities_available_on_demand) == 1
-#     assert plan.modalities_available_on_demand[0].type == ModalityType.pre_active_rest
-#
-#
-# def test_on_demand_post_active_rest_none():
-#     event_date = format_date(datetime.datetime.now())
-#     plan = DailyPlan(event_date)
-#     plan.train_later = False
-#     plan.define_available_modalities()
-#
-#     assert len(plan.modalities_available_on_demand) == 1
-#     assert plan.modalities_available_on_demand[0].type == ModalityType.post_active_rest
-#
-#
-# def test_on_demand_post_active_rest_completed():
-#     event_date_time = datetime.datetime.now()
-#     event_date = format_date(event_date_time)
-#     plan = DailyPlan(event_date)
-#     plan.train_later = False
-#     pre_active_rest = ActiveRestAfterTraining(event_date_time)
-#     pre_active_rest.completed = True
-#     plan.modalities = [pre_active_rest]
-#     plan.define_available_modalities()
-#
-#     assert len(plan.modalities_available_on_demand) == 1
-#     assert plan.modalities_available_on_demand[0].type == ModalityType.post_active_rest
+from models.daily_plan import DailyPlan
+from models.functional_movement_modalities import ModalityType, ActiveRestAfterTraining, MovementIntegrationPrep
+from utils import format_date
+import datetime
+
+
+def test_on_demand_pre_active_rest_none():
+    event_date = format_date(datetime.datetime.now())
+    plan = DailyPlan(event_date)
+    plan.define_available_modalities()
+
+    assert len(plan.modalities_available_on_demand) == 2
+    assert plan.modalities_available_on_demand[0].type == ModalityType.post_active_rest
+    assert plan.modalities_available_on_demand[1].type == ModalityType.movement_integration_prep
+
+
+def test_on_demand_movement_prep_assigned_active_rest_completed():
+    event_date_time = datetime.datetime.now()
+    event_date = format_date(event_date_time)
+    plan = DailyPlan(event_date)
+    active_rest = ActiveRestAfterTraining(event_date_time)
+    active_rest.completed = True
+    movement_prep = MovementIntegrationPrep(event_date_time)
+    plan.modalities = [active_rest, movement_prep]
+    plan.define_available_modalities()
+
+    assert len(plan.modalities_available_on_demand) == 1
+    assert plan.modalities_available_on_demand[0].type == ModalityType.post_active_rest
+
+
+def test_on_demand_movement_prep_post_active_rest_assigned():
+    event_date_time = datetime.datetime.now()
+    event_date = format_date(event_date_time)
+    plan = DailyPlan(event_date)
+    active_rest = ActiveRestAfterTraining(event_date_time)
+    movement_prep = MovementIntegrationPrep(event_date_time)
+    plan.modalities = [active_rest, movement_prep]
+    plan.define_available_modalities()
+
+    assert len(plan.modalities_available_on_demand) == 0
+
+
+def test_on_demand_movement_prep_completed_post_active_rest_assigned():
+    event_date_time = datetime.datetime.now()
+    event_date = format_date(event_date_time)
+    plan = DailyPlan(event_date)
+    active_rest = ActiveRestAfterTraining(event_date_time)
+    movement_prep = MovementIntegrationPrep(event_date_time)
+    movement_prep.completed = True
+    plan.modalities = [active_rest, movement_prep]
+    plan.define_available_modalities()
+
+    assert len(plan.modalities_available_on_demand) == 1
+    assert plan.modalities_available_on_demand[0].type == ModalityType.movement_integration_prep
