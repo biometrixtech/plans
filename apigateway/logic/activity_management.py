0e09796638dd71ac3099db3689b793098f586e17
Dipesh Gautam <dipesh@biometrixtech.com>
2020-03-19 09:34:10 -0400

    rename

--- apigateway/logic/activity_management.py
+++ apigateway/logic/activity_management.py
@@ -32,7 +32,7 @@ class ActivityManager(object):
         self.symptoms = symptoms if symptoms is not None else []
         self.historical_injury_risk_dict = None
         self.exercise_assignment_calculator = None
-        self.activity_training_sessions = [session for session in self.training_sessions]
+        self.active_training_sessions = [session for session in self.training_sessions]
         self.load_data()
 
     @xray_recorder.capture('logic.ActivityManager.load_data')
@@ -48,7 +48,7 @@ class ActivityManager(object):
 
     @xray_recorder.capture('logic.ActivityManager.save_session_dict')
     def save_session(self):
-        for session in self.activity_training_sessions:
+        for session in self.active_training_sessions:
             self.training_session_datastore.put(session)
 
     def get_session_details(self):
@@ -57,20 +57,20 @@ class ActivityManager(object):
         self.exercise_assignment_calculator.high_intensity_session = self.is_high_intensity_session()
 
     def is_high_intensity_session(self):
-        for session in self.activity_training_sessions:
+        for session in self.active_training_sessions:
             if session.ultra_high_intensity_session() and session.high_intensity_RPE():
                 return True
         return False
 
     def get_sport_body_parts(self):
         sport_body_parts = {}
-        for session in self.activity_training_sessions:
+        for session in self.active_training_sessions:
             sport_body_parts.update(session.get_load_body_parts())
         return sport_body_parts
 
     def check_cardio_sport(self):
         sport_cardio_plyometrics = False
-        for session in self.activity_training_sessions:
+        for session in self.active_training_sessions:
             if session.is_cardio_plyometrics():
                 sport_cardio_plyometrics = True
         return sport_cardio_plyometrics
@@ -119,7 +119,7 @@ class ActivityManager(object):
         self.prepare_data(8)  # we don't want to persist this RPE
         self.get_session_details()
         movement_prep = self.exercise_assignment_calculator.get_movement_prep(self.athlete_id, force_on_demand=force_on_demand)
-        movement_prep.training_session_id = self.activity_training_sessions[0].id
+        movement_prep.training_session_id = self.active_training_sessions[0].id
         self.movement_prep_datastore.put(movement_prep)
         self.save_session()
 
@@ -133,7 +133,7 @@ class ActivityManager(object):
         """
         self.prepare_data(5)  # we don't want to persist this RPE
         mobility_wod = self.exercise_assignment_calculator.get_mobility_wod(self.athlete_id, force_on_demand=force_on_demand)
-        mobility_wod.training_session_ids = [session.id for session in self.activity_training_sessions]
+        mobility_wod.training_session_ids = [session.id for session in self.active_training_sessions]
         self.mobility_wod_datastore.put(mobility_wod)
         self.save_session()
 
@@ -148,13 +148,13 @@ class ActivityManager(object):
         :return responsive_recovery: ResponsiveRecovery
         """
         if training_session_id is not None:
-            self.activity_training_sessions = [session for session in self.training_sessions if session.id == training_session_id]
+            self.active_training_sessions = [session for session in self.training_sessions if session.id == training_session_id]
         self.prepare_data(None)
         self.get_session_details()
         responsive_recovery = self.exercise_assignment_calculator.get_responsive_recovery(self.athlete_id, force_on_demand=force_on_demand)
         if responsive_recovery_id is not None:
             responsive_recovery.responsive_recovery_id = responsive_recovery_id
-        responsive_recovery.training_session_id = self.activity_training_sessions[0].id
+        responsive_recovery.training_session_id = self.active_training_sessions[0].id
         self.responsive_recovery_datastore.put(responsive_recovery)
         self.save_session()
 
