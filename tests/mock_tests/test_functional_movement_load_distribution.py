b911f5fb0ae04b8e8238e28caaaccdb5daffae91
Dipesh Gautam <dipesh@biometrixtech.com>
2020-04-09 11:40:37 -0400

    max_load instead of total_load

--- tests/mock_tests/test_functional_movement_load_distribution.py
+++ tests/mock_tests/test_functional_movement_load_distribution.py
@@ -43,42 +43,48 @@ def get_exercise(reps=1, sets=1, unit=UnitOfMeasure.seconds, movement_id=""):
     return exercise
 
 
-def test_run():
-    exercise = get_exercise(reps=3000, sets=1, unit=UnitOfMeasure.seconds, movement_id="5823768d473c06100052ed9a")  # run
-    create_and_process_wokout([exercise])
+def get_max_action_load(functional_movement_action_mapping):
+    max_action_load = 0
+    for muscle_load in functional_movement_action_mapping.muscle_load.values():
+        max_action_load = max([max_action_load, muscle_load.eccentric_load, muscle_load.concentric_load])
+    return max_action_load
 
+
+def get_max_load(exercise):
     factory = FunctionalMovementFactory()
     functional_movement_dict = factory.get_functional_movement_dictinary()
-    total_loads = {}
+    max_loads = {}
     for exercise_action in exercise.primary_actions:
         try:
             functional_movement_action_mapping = FunctionalMovementActionMapping(exercise_action, {}, datetime.datetime.now(), functional_movement_dict)
-            action_load = 0
-            for muscle_load in functional_movement_action_mapping.muscle_load.values():
-                action_load += muscle_load.eccentric_load
-                action_load += muscle_load.concentric_load
-            total_loads[exercise_action.id] = action_load
+            max_action_load = get_max_action_load(functional_movement_action_mapping)
+            max_loads[exercise_action.id] = max_action_load
         except:
             pass
-    assert total_loads['1_a'] > total_loads ['1_b'] > total_loads['1_c']
+    return max_loads
+
+
+def test_run():
+    exercise = get_exercise(reps=3000, sets=1, unit=UnitOfMeasure.seconds, movement_id="5823768d473c06100052ed9a")  # run
+    create_and_process_wokout([exercise])
+    max_loads = get_max_load(exercise)
+
+    # factory = FunctionalMovementFactory()
+    # functional_movement_dict = factory.get_functional_movement_dictinary()
+    # for exercise_action in exercise.primary_actions:
+    #     try:
+    #         functional_movement_action_mapping = FunctionalMovementActionMapping(exercise_action, {}, datetime.datetime.now(), functional_movement_dict)
+    #         max_action_load = get_max_load(ex)
+    #         max_loads[exercise_action.id] = max_action_load
+    #     except:
+    #         pass
+    assert max_loads['1_a'] > max_loads['1_b'] > max_loads['1_c']
 
 
 def test_sprint():
     exercise = get_exercise(reps=3000, sets=1, unit=UnitOfMeasure.seconds, movement_id="58459de6dc2ce90011f93d86")  # sprint
     create_and_process_wokout([exercise])
+    max_loads = get_max_load(exercise)
 
-    factory = FunctionalMovementFactory()
-    functional_movement_dict = factory.get_functional_movement_dictinary()
-    total_loads = {}
-    for exercise_action in exercise.primary_actions:
-        try:
-            functional_movement_action_mapping = FunctionalMovementActionMapping(exercise_action, {}, datetime.datetime.now(), functional_movement_dict)
-            action_load = 0
-            for muscle_load in functional_movement_action_mapping.muscle_load.values():
-                action_load += muscle_load.eccentric_load
-                action_load += muscle_load.concentric_load
-            total_loads[exercise_action.id] = action_load
-        except:
-            pass
-    assert total_loads['2_a'] > total_loads ['2_b'] > total_loads['2_c']
+    assert max_loads['2_a'] > max_loads['2_b'] > max_loads['2_c']
 
