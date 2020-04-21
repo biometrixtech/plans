54e69e36c9fecc59b1287ef98bcee4404a2023b0
Dipesh Gautam <dipesh@biometrixtech.com>
2020-04-21 10:16:04 -0400

    modality available if no active one is present

--- tests/mock_tests/test_templated_modalities.py
+++ tests/mock_tests/test_templated_modalities.py
@@ -40,6 +40,20 @@ def test_on_demand_movement_prep_post_active_rest_assigned():
     assert len(plan.modalities_available_on_demand) == 0
 
 
+def test_on_demand_movement_prep_inactive_post_active_rest_assigned():
+    event_date_time = datetime.datetime.now()
+    event_date = format_date(event_date_time)
+    plan = DailyPlan(event_date)
+    active_rest = ActiveRestAfterTraining(event_date_time)
+    movement_prep = MovementIntegrationPrepModality(event_date_time)
+    movement_prep.active = False
+    plan.modalities = [active_rest, movement_prep]
+    plan.define_available_modalities()
+
+    assert len(plan.modalities_available_on_demand) == 1
+    assert plan.modalities_available_on_demand[0].type == ModalityType.movement_integration_prep
+
+
 def test_on_demand_movement_prep_completed_post_active_rest_assigned():
     event_date_time = datetime.datetime.now()
     event_date = format_date(event_date_time)
