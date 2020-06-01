7c931ec00951d148833e41267bcac2c0e8516d98
Paul LaForge <paul@biometrixtech.com>
2020-06-01 11:02:36 -0600

    userstats attributes for baseline and load analysis

--- apigateway/models/user_stats.py
+++ apigateway/models/user_stats.py
@@ -1,6 +1,7 @@
 from serialisable import Serialisable
 from models.load_stats import LoadStats
 from models.session import HighLoadSession, HighDetailedLoadSession
+from models.stats import StandardErrorRange
 from utils import format_date, parse_date, format_datetime, parse_datetime
 from fathomapi.utils.exceptions import InvalidSchemaException
 
@@ -19,6 +20,30 @@ class UserStats(Serialisable):
 
         self.expected_weekly_workouts = 3
 
+        self.vo2_max = None
+        self.functional_threshold_power = None
+
+        self.average_newtons_5_day = None
+        self.average_newtons_20_day = None
+        self.average_watts_5_day = None
+        self.average_watts_20_day = None
+        self.average_work_vo2_5_day = None
+        self.average_work_vo2_20_day = None
+        self.average_rpe_5_day = None
+        self.average_rpe_20_day = None
+
+        self.average_newtons_load_5_day = None
+        self.average_newtons_load_20_day = None
+        self.average_watts_load_5_day = None
+        self.average_watts_load_20_day = None
+        self.average_work_vo2_load_5_day = None
+        self.average_work_vo2_load_20_day = None
+        self.average_rpe_load_5_day = None
+        self.average_rpe_load_20_day = None
+
+        self.average_trimp_5_day = None
+        self.average_trimp_20_day = None
+
     def __setattr__(self, name, value):
         if name == 'event_date' and value is not None:
             if isinstance(value, str):
@@ -27,6 +52,85 @@ class UserStats(Serialisable):
                 except InvalidSchemaException:
                     value = parse_date(value)
         super().__setattr__(name, value)
+
+    def newtons_load_5_20(self):
+
+        if self.average_newtons_load_5_day is not None and self.average_newtons_load_20_day is not None:
+            standard_error_range = StandardErrorRange()
+            standard_error_range.lower_bound = self.average_newtons_load_5_day.lower_bound / self.average_newtons_load_20_day.lower_bound
+            standard_error_range.upper_bound = self.average_newtons_load_5_day.upper_bound / self.average_newtons_load_20_day.upper_bound
+            standard_error_range.observed_value = self.average_newtons_load_5_day.observed_value / self.average_newtons_load_20_day.observed_value
+            standard_error_range.insufficient_data = min(self.average_newtons_load_5_day.insufficient_data, self.average_newtons_load_20_day.insufficient_data)
+
+            return standard_error_range
+
+        else:
+
+            return None
+
+    def rpe_load_5_20(self):
+
+        if self.average_rpe_load_5_day is not None and self.average_rpe_load_20_day is not None:
+            standard_error_range = StandardErrorRange()
+            standard_error_range.lower_bound = self.average_rpe_load_5_day.lower_bound / self.average_rpe_load_20_day.lower_bound
+            standard_error_range.upper_bound = self.average_rpe_load_5_day.upper_bound / self.average_rpe_load_20_day.upper_bound
+            standard_error_range.observed_value = self.average_rpe_load_5_day.observed_value / self.average_rpe_load_20_day.observed_value
+            standard_error_range.insufficient_data = min(self.average_rpe_load_5_day.insufficient_data,
+                                                         self.average_rpe_load_20_day.insufficient_data)
+
+            return standard_error_range
+
+        else:
+
+            return None
+
+    def trimp_5_20(self):
+
+        if self.average_trimp_5_day is not None and self.average_trimp_20_day is not None:
+            standard_error_range = StandardErrorRange()
+            standard_error_range.lower_bound = self.average_trimp_5_day.lower_bound / self.average_trimp_20_day.lower_bound
+            standard_error_range.upper_bound = self.average_trimp_5_day.upper_bound / self.average_trimp_20_day.upper_bound
+            standard_error_range.observed_value = self.average_trimp_5_day.observed_value / self.average_trimp_20_day.observed_value
+            standard_error_range.insufficient_data = min(self.average_trimp_5_day.insufficient_data,
+                                                         self.average_trimp_20_day.insufficient_data)
+
+            return standard_error_range
+
+        else:
+
+            return None
+
+    def watts_load_5_20(self):
+
+        if self.average_watts_load_5_day is not None and self.average_watts_load_20_day is not None:
+            standard_error_range = StandardErrorRange()
+            standard_error_range.lower_bound = self.average_watts_load_5_day.lower_bound / self.average_watts_load_20_day.lower_bound
+            standard_error_range.upper_bound = self.average_watts_load_5_day.upper_bound / self.average_watts_load_20_day.upper_bound
+            standard_error_range.observed_value = self.average_watts_load_5_day.observed_value / self.average_watts_load_20_day.observed_value
+            standard_error_range.insufficient_data = min(self.average_watts_load_5_day.insufficient_data,
+                                                         self.average_watts_load_20_day.insufficient_data)
+
+            return standard_error_range
+
+        else:
+
+            return None
+
+    def work_vo2_load_5_20(self):
+
+        if self.average_work_vo2_5_day is not None and self.average_work_vo2_load_20_day is not None:
+            standard_error_range = StandardErrorRange()
+            standard_error_range.lower_bound = self.average_work_vo2_5_day.lower_bound / self.average_work_vo2_load_20_day.lower_bound
+            standard_error_range.upper_bound = self.average_work_vo2_5_day.upper_bound / self.average_work_vo2_load_20_day.upper_bound
+            standard_error_range.observed_value = self.average_work_vo2_5_day.observed_value / self.average_work_vo2_load_20_day.observed_value
+            standard_error_range.insufficient_data = min(self.average_work_vo2_5_day.insufficient_data,
+                                                         self.average_work_vo2_load_20_day.insufficient_data)
+
+            return standard_error_range
+
+        else:
+
+            return None
 
     def json_serialise(self):
         ret = {
@@ -37,7 +141,27 @@ class UserStats(Serialisable):
             'eligible_for_high_load_trigger': self.eligible_for_high_load_trigger,
             'sport_max_load': {str(sport_name): sport_max_load.json_serialise() for (sport_name, sport_max_load) in self.sport_max_load.items()},
             'api_version': self.api_version,
-            'timezone': self.timezone
+            'timezone': self.timezone,
+            'vo2_max': self.vo2_max,
+            'functional_threshold_power': self.functional_threshold_power,
+            'average_newtons_5_day': self.average_newtons_5_day.json_serialise() if self.average_newtons_5_day is not None else None,
+            'average_newtons_20_day': self.average_newtons_20_day.json_serialise() if self.average_newtons_20_day is not None else None,
+            'average_watts_5_day': self.average_watts_5_day.json_serialise() if self.average_watts_5_day is not None else None,
+            'average_watts_20_day': self.average_watts_20_day.json_serialise() if self.average_watts_20_day is not None else None,
+            'average_work_vo2_5_day': self.average_work_vo2_5_day.json_serialise() if self.average_work_vo2_5_day is not None else None,
+            'average_work_vo2_20_day': self.average_work_vo2_20_day.json_serialise() if self.average_work_vo2_20_day is not None else None,
+            'average_rpe_5_day': self.average_rpe_5_day.json_serialise() if self.average_rpe_5_day is not None else None,
+            'average_rpe_20_day': self.average_rpe_20_day.json_serialise() if self.average_rpe_20_day is not None else None,
+            'average_newtons_load_5_day': self.average_newtons_load_5_day.json_serialise() if self.average_newtons_load_5_day is not None else None,
+            'average_newtons_load_20_day': self.average_newtons_load_20_day.json_serialise() if self.average_newtons_load_20_day is not None else None,
+            'average_watts_load_5_day': self.average_watts_load_5_day.json_serialise() if self.average_watts_load_5_day is not None else None,
+            'average_watts_load_20_day': self.average_watts_load_20_day.json_serialise() if self.average_watts_load_20_day is not None else None,
+            'average_work_vo2_load_5_day': self.average_work_vo2_load_5_day.json_serialise() if self.average_work_vo2_load_5_day is not None else None,
+            'average_work_vo2_load_20_day': self.average_work_vo2_load_20_day.json_serialise() if self.average_work_vo2_load_20_day is not None else None,
+            'average_rpe_load_5_day': self.average_rpe_load_5_day.json_serialise() if self.average_rpe_load_5_day is not None else None,
+            'average_rpe_load_20_day': self.average_rpe_load_20_day.json_serialise() if self.average_rpe_load_20_day is not None else None,
+            'average_trimp_5_day': self.average_trimp_5_day.json_serialise() if self.average_trimp_5_day is not None else None,
+            'average_trimp_20_day': self.average_trimp_20_day.json_serialise() if self.average_trimp_20_day is not None else None
         }
         return ret
 
@@ -51,6 +175,26 @@ class UserStats(Serialisable):
         user_stats.high_relative_load_sessions = [HighLoadSession.json_deserialise(session) if 'sport_name' in session else HighDetailedLoadSession.json_deserialise(session) for session in input_dict.get('high_relative_load_sessions', [])]
         user_stats.eligible_for_high_load_trigger = input_dict.get('eligible_for_high_load_trigger', False)
         user_stats.sport_max_load = {int(sport_name): SportMaxLoad.json_deserialise(sport_max_load) for (sport_name, sport_max_load) in input_dict.get('sport_max_load', {}).items()}
+        user_stats.vo2_max = input_dict.get('vo2_max')
+        user_stats.functional_threshold_power = input_dict.get('functional_threshold_power')
+        user_stats.average_newtons_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_newtons_5_day') is not None else None
+        user_stats.average_newtons_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_newtons_20_day') is not None else None
+        user_stats.average_watts_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_watts_5_day') is not None else None
+        user_stats.average_watts_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_watts_20_day') is not None else None
+        user_stats.average_work_vo2_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_work_vo2_5_day') is not None else None
+        user_stats.average_work_vo2_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_work_vo2_20_day') is not None else None
+        user_stats.average_rpe_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_rpe_5_day') is not None else None
+        user_stats.average_rpe_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_rpe_20_day') is not None else None
+        user_stats.average_newtons_load_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_newtons_load_5_day') is not None else None
+        user_stats.average_newtons_load_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_newtons_load_20_day') is not None else None
+        user_stats.average_watts_load_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_watts_load_5_day') is not None else None
+        user_stats.average_watts_load_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_watts_load_20_day') is not None else None
+        user_stats.average_work_vo2_load_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_work_vo2_load_5_day') is not None else None
+        user_stats.average_work_vo2_load_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_work_vo2_load_20_day') is not None else None
+        user_stats.average_rpe_load_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_rpe_load_5_day') is not None else None
+        user_stats.average_rpe_load_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_rpe_load_20_day') is not None else None
+        user_stats.average_trimp_5_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_trimp_5_day') is not None else None
+        user_stats.average_trimp_20_day = StandardErrorRange.json_deserialise(input_dict) if input_dict.get('average_trimp_20_day') is not None else None
 
         return user_stats
 
