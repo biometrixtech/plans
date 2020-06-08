2272b4ea7a463688b056dc4eb0cad349e9c1a3a1
Paul LaForge <paul@biometrixtech.com>
2020-06-08 09:24:25 -0600

    set up session_load_datastore

--- apigateway/models/training_load.py
+++ apigateway/models/training_load.py
@@ -1,4 +1,6 @@
 from models.training_volume import StandardErrorRange
+from serialisable import Serialisable
+from utils import format_datetime, parse_datetime
 
 
 class TrainingLoad(object):
@@ -26,11 +28,31 @@ class TrainingLoad(object):
             self.power_load.add(power_load)
 
 
-class SessionLoad(TrainingLoad):
+class SessionLoad(Serialisable, TrainingLoad):
     def __init__(self, session_id, user_id, event_date_time):
         super().__init__()
         self.session_id = session_id
         self.user_id = user_id
         self.event_date_time = event_date_time
+
+    def json_serialise(self):
+        ret = {
+            'session_id': self.session_id,
+            'user_id': self.user_id,
+            'event_date_time': format_datetime(self.event_date_time),
+            'tissue_load': self.tissue_load.json_serialise() if self.tissue_load is not None else None,
+            'force_load': self.force_load.json_serialise() if self.force_load is not None else None,
+            'power_load': self.power_load.json_serialise() if self.power_load is not None else None
+        }
+        return ret
+
+    @classmethod
+    def json_deserialise(cls, input_dict):
+        session_load = cls(session_id=input_dict['session_id'], user_id=input_dict['user_id'], event_date_time=parse_datetime(input_dict['event_date_time']))
+        session_load.tissue_load.json_deserialise(input_dict['tissue_load'])
+        session_load.force_load.json_deserialise(input_dict['force_load'])
+        session_load.power_load.json_deserialise(input_dict['power_load'])
+
+        return session_load
 
 
