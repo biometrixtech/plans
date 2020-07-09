8049a80a9cb1914ac6b309e9998bb382d9fc11b8
Paul LaForge <paul@biometrixtech.com>
2020-07-09 12:54:44 -0600

    fixes

--- tests/mock_tests/test_detailed_adaptation_type.py
+++ tests/mock_tests/test_detailed_adaptation_type.py
@@ -96,6 +96,37 @@ def process_adaptation_types_no_reps(action_list, rpe_list, duration=None):
     return detailed_load_processor
 
 
+# def process_adaptation_types_duration(action_list, duration):
+#
+#     detailed_load_processor = DetailedLoadProcessor()
+#
+#     functional_movement_factory = FunctionalMovementFactory()
+#     functional_movement_dict = functional_movement_factory.get_functional_movement_dictinary()
+#     event_date = datetime.now()
+#
+#     for action in action_list:
+#         duration = duration
+#
+#         base_exercise = BaseWorkoutExercise()
+#         base_exercise.training_type = action.training_type
+#         base_exercise.power_load = StandardErrorRange(lower_bound=50, observed_value=75, upper_bound=100)
+#         base_exercise.set_adaption_type()
+#
+#         action.power_load_left = base_exercise.power_load
+#         action.power_load_right = base_exercise.power_load
+#
+#         functional_movement_action_mapping = FunctionalMovementActionMapping(action,
+#                                                                              {},
+#                                                                              event_date, functional_movement_dict)
+#         #for muscle_string, load in functional_movement_action_mapping.muscle_load.items():
+#         detailed_load_processor.add_load(functional_movement_action_mapping,
+#                                          adaptation_type=base_exercise.adaptation_type,
+#                                          movement_action=action,
+#                                          training_load_range=base_exercise.power_load,
+#                                          duration=duration)
+#
+#     return detailed_load_processor
+
 def process_adaptation_types_percent_max_hr(action_list, percent_max_hr_list, duration=None):
 
     detailed_load_processor = DetailedLoadProcessor()
@@ -182,26 +213,26 @@ def test_anaerobic_interval_training_detection():
     no_resistance_length = len(no_resistance_list)
 
     assert detailed_load_processor.session_detailed_load.anaerobic_interval_training is not None
-#
-# def test_stabilization_endurance_detection():
-#     # should go to Stabilization Endurance, Stabilization Strength, Functional Strength, Strength Endurance
-#     action_list = get_filtered_actions([TrainingType.strength_integrated_resistance, TrainingType.strength_endurance])
-#
-#     no_speed_list = [a for a in action_list if a.speed is None]
-#     no_resistance_list = [a for a in action_list if a.resistance is None]
-#     slow_tempo_list = [a for a in action_list if a.speed == MovementSpeed.slow]
-#
-#     reps_list = [15]
-#     rpes = [6]
-#
-#     detailed_load_processor = process_adaptation_types(action_list, reps_list, rpes)
-#
-#     action_list_length = len(action_list)
-#     no_speed_length = len(no_speed_list)
-#     no_resistance_length = len(no_resistance_list)
-#
-#     assert 0 < len(slow_tempo_list)
-#     assert detailed_load_processor.session_detailed_load.stabilization_endurance is not None
+
+def test_stabilization_endurance_detection():
+    # should go to Stabilization Endurance, Stabilization Strength, Functional Strength, Strength Endurance
+    action_list = get_filtered_actions([TrainingType.strength_integrated_resistance, TrainingType.strength_endurance])
+
+    no_speed_list = [a for a in action_list if a.speed is None]
+    no_resistance_list = [a for a in action_list if a.resistance is None]
+    slow_tempo_list = [a for a in action_list if a.speed == MovementSpeed.slow]
+
+    reps_list = [15]
+    rpes = [6]
+
+    detailed_load_processor = process_adaptation_types(action_list, reps_list, rpes)
+
+    action_list_length = len(action_list)
+    no_speed_length = len(no_speed_list)
+    no_resistance_length = len(no_resistance_list)
+
+    #assert 0 < len(slow_tempo_list)
+    assert detailed_load_processor.session_detailed_load.stabilization_endurance is not None
 
 
 def test_stabilization_strength_detection():
@@ -304,25 +335,25 @@ def test_muscular_endurance_cardio_detection():
     assert detailed_load_processor.session_detailed_load.muscular_endurance is not None
 
 
-# def test_muscular_endurance_strength_detection():
-#     # should go to Stabilization Endurance, Stabilization Strength, Functional Strength, Strength Endurance
-#     action_list = get_filtered_actions([TrainingType.strength_cardiorespiratory])
-#
-#     no_speed_list = [a for a in action_list if a.speed is None]
-#     no_resistance_list = [a for a in action_list if a.resistance is None]
-#     slow_tempo_list = [a for a in action_list if a.speed == MovementSpeed.slow]
-#
-#     rpes = [6]
-#     reps = [15]
-#
-#     detailed_load_processor = process_adaptation_types(action_list, reps_list=reps, rpe_list=rpes)
-#
-#     action_list_length = len(action_list)
-#     no_speed_length = len(no_speed_list)
-#     no_resistance_length = len(no_resistance_list)
-#
-#     assert 0 < len(slow_tempo_list)
-#     assert detailed_load_processor.session_detailed_load.muscular_endurance is not None
+def test_muscular_endurance_strength_detection():
+    # should go to Stabilization Endurance, Stabilization Strength, Functional Strength, Strength Endurance
+    action_list = get_filtered_actions([TrainingType.strength_cardiorespiratory])
+
+    no_speed_list = [a for a in action_list if a.speed is None]
+    no_resistance_list = [a for a in action_list if a.resistance is None]
+    slow_tempo_list = [a for a in action_list if a.speed in [MovementSpeed.slow, MovementSpeed.none]]
+
+    rpes = [6]
+    reps = [15]
+
+    detailed_load_processor = process_adaptation_types_no_reps(action_list, rpes,duration=300)
+
+    action_list_length = len(action_list)
+    no_speed_length = len(no_speed_list)
+    no_resistance_length = len(no_resistance_list)
+
+    #assert 0 < len(slow_tempo_list)
+    assert detailed_load_processor.session_detailed_load.muscular_endurance is not None
 
 
 def test_strength_endurance_detection():
