31cabbe9dbd686616d30fc1c59782e2d9b708fee
Dipesh Gautam <dipesh@biometrixtech.com>
2020-07-14 18:58:47 +0000

    save without unzipping

--- apigateway/routes/workout_performance_data.py
+++ apigateway/routes/workout_performance_data.py
@@ -5,11 +5,16 @@ from fathomapi.utils.exceptions import InvalidSchemaException, ApplicationExcept
 from fathomapi.utils.xray import xray_recorder
 
 # import boto3
-import zipfile
+# import zipfile
+import io
+import boto3
+from boto3.s3.transfer import TransferConfig, S3Transfer
 
 app = Blueprint('performance_data', __name__)
 
-
+_ingest_s3_bucket = boto3.resource('s3').Bucket('fathom-otf')
+# Need to use single threading to prevent X Ray tracing errors
+_s3_config = TransferConfig(use_threads=False)
 
 @app.route('/<uuid:user_id>/upload', methods=['POST'])
 @require.authenticated.any
@@ -18,8 +23,12 @@ def handle_performance_data_upload(user_id):
     print(user_id)
 
     if request.headers['Content-Type'] == 'application/zip':
-        data = request.get_data()
-        # zipfiles = zipfile.ZipFile(data)
+        data = request.get_data().encode()
+        print(type(data))
+        # f = io.StringIO(data)
+        f = io.BytesIO(data)
+        _ingest_s3_bucket.upload_fileobj(f, 'test_file.zip', Config=_s3_config)
+
         # print(zipfiles)
 
         # from cStringIO import StringIO
