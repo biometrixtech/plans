b926c47418c499b83e156899438eb09a1ddf3332
Dipesh Gautam <dipesh@biometrixtech.com>
2020-07-15 13:00:41 +0000

    octet stream

--- apigateway/routes/workout_performance_data.py
+++ apigateway/routes/workout_performance_data.py
@@ -1,14 +1,16 @@
 from flask import request, Blueprint
 
 from fathomapi.utils.decorators import require
-from fathomapi.utils.exceptions import InvalidSchemaException, ApplicationException
+from fathomapi.utils.exceptions import ApplicationException
 from fathomapi.utils.xray import xray_recorder
 
 # import boto3
 # import zipfile
 import io
 import boto3
-from boto3.s3.transfer import TransferConfig, S3Transfer
+from boto3.s3.transfer import TransferConfig
+import base64
+
 
 app = Blueprint('performance_data', __name__)
 
@@ -20,22 +22,17 @@ _s3_config = TransferConfig(use_threads=False)
 @require.authenticated.any
 @xray_recorder.capture('routes.performance_data.upload')
 def handle_performance_data_upload(user_id):
-    print(user_id)
-
-    if request.headers['Content-Type'] == 'application/zip':
-        data = request.get_data().encode()
-        print(type(data))
-        # f = io.StringIO(data)
+    # if request.headers['Content-Type'] == 'application/zip':
+    #     # data = request.get_data().encode()
+    #     data = base64.b64decode(request.get_data())
+    #     print(type(data))
+    #     # f = io.StringIO(data)
+    #     f = io.BytesIO(data)
+    #     _ingest_s3_bucket.upload_fileobj(f, 'test_file.zip', Config=_s3_config)
+    if request.headers['Content-Type'] == 'application/octet-stream':
+        data = base64.b64decode(request.get_data())
         f = io.BytesIO(data)
-        _ingest_s3_bucket.upload_fileobj(f, 'test_file.zip', Config=_s3_config)
-
-        # print(zipfiles)
-
-        # from cStringIO import StringIO
-        # import zipfile
-        # zfp = zipfile.ZipFile(StringIO(data), "r")
-
-
+        _ingest_s3_bucket.upload_fileobj(f, 'test_file_octet.zip', Config=_s3_config)
     else:
         raise ApplicationException(
             415,
