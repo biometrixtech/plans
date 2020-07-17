6e3b1901bce029e652275dd690d0debcbb9d5e73
Dipesh Gautam <dipesh@biometrixtech.com>
2020-07-17 19:44:45 +0000

    try unzipping

--- apigateway/parse_performance_data.py
+++ apigateway/parse_performance_data.py
@@ -1,49 +1,36 @@
-import json
-import urllib
 import boto3
-import logging
 import io
 import pandas as pd
 import numpy as np
+import zipfile
 
 
+def lambda_handler(event, _):
+    print(f"Starting unzipping for file {event['Records'][0]['s3']['object']['key']}")
+    s3_key = ""
+    s3_bucket = ""
+    try:
+        print(event)
+        record = event['Records'][0]
+        s3_bucket = record['s3']['bucket']['name']
+        s3_key = record['s3']['object']['key'].split('/')[-1]
+        s3_resource = boto3.resource('s3')
+        s3_object = s3_resource.Object(s3_bucket, record['s3']['object']['key'])
 
-def lambda_handler(event, context):
-    print('invoked')
-    print(event, context)
-    print(np.__version__)
-    print(pd.__version__)
-    
-    # logger.info('Starting unzipping for file' + urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).encode('utf8'))
-    
-    # try:
-    #     cont_write = 'fathom-otf'
-    #     bucket = event['Records'][0]['s3']['bucket']['name']
-    #     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).encode('utf8')
-    #     s3r = boto3.resource('s3')
-
-    #     logger.info('Obtained S3 Resource')
-    #     obj = s3r.Bucket(bucket).Object(key)
-    #     logger.info('Obtained Key')        
-    #     fileobj = obj.get()
-    #     logger.info('Got Fileobj')        
-    #     body = fileobj["Body"].read()
-    #     logger.info('Read Content')        
-    #     content = cStringIO.StringIO(body)
-    #     logger.info('Converted Content')
-    #     zipped = zf.ZipFile(content)
-    #     names = zipped.namelist()
-    #     # unzipped_content = cStringIO.StringIO()
-    #     for name in names:
-    #         unzipped_content = zipfiles.open(name).read()
-    #         logger.info('Unzipped File')
-    #         # data = pd.read_csv(unzipped_content)
-    #         # f = cStringIO.StringIO()
-    #         # data.to_csv(f, index = False)
-    #         # f.seek(0)
-    #         s3r.Bucket(cont_write).put_object(Key='unzipped/'+key+'.csv', Body=unzipped_content)
-    #     logger.info('Wrote File to processed container')
-    # except Exception as e:
-    #     logger.info(e)
-    #     logger.info('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-    #     raise e
+        fileobj = s3_object.get()
+        print('Got Fileobj')
+        body = fileobj["Body"].read()
+        print('Read Content')
+        content = io.BytesIO(body)
+        print('Converted Content')
+        unzipped_files = zipfile.ZipFile(content)
+        names = unzipped_files.namelist()
+        for name in names:
+            unzipped_content = unzipped_files.open(name).read()
+            print('Unzipped File')
+            s3_resource.Bucket(s3_bucket).put_object(Key='unzipped/'+s3_key+'.csv', Body=unzipped_content)
+        print('Wrote File to processed container')
+    except Exception as e:
+        print(e)
+        print(f'Error getting object {s3_key} from bucket {s3_bucket}. Make sure they exist and your bucket is in the same region as this function.')
+        raise e
