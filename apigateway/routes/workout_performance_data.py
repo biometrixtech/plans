2fec3f938187c18bc217d9b9521cf19c90d6a4bc
Dipesh Gautam <dipesh@biometrixtech.com>
2020-07-22 16:45:23 +0000

    templated s3 bucket

--- apigateway/routes/workout_performance_data.py
+++ apigateway/routes/workout_performance_data.py
@@ -4,21 +4,30 @@ import uuid
 import boto3
 from boto3.s3.transfer import TransferConfig
 import base64
+import datetime
+
+from fathomapi.api.config import Config
 from fathomapi.utils.decorators import require
 from fathomapi.utils.exceptions import ApplicationException
 from fathomapi.utils.xray import xray_recorder
+from utils import format_datetime
 
 
 app = Blueprint('performance_data', __name__)
 
 
-@app.route('/<uuid:user_id>/upload', methods=['PUT'])
+@app.route('/<uuid:user_id>/<str:program_id>/upload', methods=['PUT'])
 @require.authenticated.any
 @xray_recorder.capture('routes.performance_data.upload')
-def handle_performance_data_upload(user_id):
-    session_id = str(uuid.uuid4())
+def handle_performance_data_upload(user_id, program_id):
+    event_date_time = format_datetime(datetime.datetime.now())
+    unique_key = f'http://session.fathomai.com/{user_id}_{program_id}_{event_date_time}'
+    session_id = str(uuid.uuid5(uuid.NAMESPACE_URL, unique_key))
 
-    _ingest_s3_bucket = boto3.resource('s3').Bucket('fathom-otf2')
+    # session_id = str(uuid.uuid4())
+
+    _ingest_s3_bucket = boto3.resource('s3').Bucket(Config.get('PERFORMANCE_DATA_S3_BUCKET'))
+    # _ingest_s3_bucket = boto3.resource('s3').Bucket('fathom-otf2')
     # Need to use single threading to prevent X Ray tracing errors
     _s3_config = TransferConfig(use_threads=False)
     if request.headers['Content-Type'] == 'application/octet-stream':
